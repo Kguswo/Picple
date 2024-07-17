5cc33f90f5dfe70286c4a0be822a180b5d6f675c
염규영 <yeomky98@naver.com>
2024-07-17 17:48:29 +0900

    [#6] rename: rename files

--- backend/src/main/java/com/ssafy/picple/domain/board/entity/Board.java
+++ backend/src/main/java/com/ssafy/picple/domain/board/entity/Board.java
@@ -1,32 +1,40 @@
 package com.ssafy.picple.domain.board.entity;
 
-import com.ssafy.picple.domain.User.entity.User;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
+
+import com.ssafy.picple.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "board")
 @Getter
 public class Board {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
-    @Column(nullable = false)
-    private int hit;
+	@Column(nullable = false)
+	private int hit;
 
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
 
-    @Column(nullable = false)
-    private boolean isDeleted;
+	@Column(nullable = false)
+	private boolean isDeleted;
 
 }
--- backend/src/main/java/com/ssafy/picple/domain/calendar/entity/Calendar.java
+++ backend/src/main/java/com/ssafy/picple/domain/calendar/entity/Calendar.java
@@ -1,9 +1,15 @@
 package com.ssafy.picple.domain.calendar.entity;
 
-import com.ssafy.picple.domain.User.entity.User;
 import com.ssafy.picple.domain.photo.entity.Photo;
-import jakarta.persistence.*;
+import com.ssafy.picple.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -12,16 +18,16 @@ import lombok.Getter;
 @Getter
 public class Calendar {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "photo_id", nullable = false)
-    private Photo photo;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "photo_id", nullable = false)
+	private Photo photo;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
 
 }
