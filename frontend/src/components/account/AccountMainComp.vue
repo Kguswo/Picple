b5680fc837070010c0e51bd4b8ab31c112fa4e2e
김예훈 <rladpgns00@gmail.com>
2024-07-17 17:12:18 +0900

    [#8] design: 로그인, 회원가입, 정보 수정, 비밀번호 찾기, 비밀번호 변경 화면 관련 View, Component 추가

---
+++ frontend/src/components/account/AccountMainComp.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div class="content">
+        <div id="title">{{ title }}</div>
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.content {
+    display: flex;
+    justify-content: space-around;
+    width: 700px;
+}
+
+#title {
+    font-size: 50px;
+    margin: auto 0;
+}
+</style>
