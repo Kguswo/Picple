f527ebb84eff1e7220b36c97a09fde72d687438b
dbqudwn011 <dbqudwn011@naver.com>
2024-07-17 17:44:30 +0900

    [#7] design: MainView 제작

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,84 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="logo-container">
+      <img src="@/assets/img/mainView/picpleLogo.png" class="logo">
+    </div>
+
+
+    <div class="cardBox">
+
+      <div class="card">
+        <div class="front">
+          <div class="imageincard">
+            <img src="@/assets/img/mainView/camera.jpg" class="card-img-top" alt="..." />
+          </div>
+
+          <div class="card-body">
+            <h3 class="card-title">부스 생성</h3>
+
+          </div>
+        </div>
+        <div class="back">
+          부스 생성 설명
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="front">
+          <div class="imageincard">
+            <img src="@/assets/img/mainView/noimage.png" class="card-img-top" alt="..." />
+          </div>
+          <div class="card-body">
+            <h3 class="card-title">부스 참여</h3>
+          </div>
+        </div>
+        <div class="back">
+          부스 참여 설명
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="front">
+          <div class="imageincard">
+            <img src="@/assets/img/mainView/board.jpg" class="card-img-top" alt="..." />
+          </div>
+
+          <div class="card-body">
+            <h3 class="card-title"> 게시판</h3>
+          </div>
+        </div>
+
+
+        <div class="back">
+          게시판 설명
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="front">
+          <div class="imageincard">
+            <img src="@/assets/img/mainView/calender.jpg" class="card-img-top" alt="..." />
+          </div>
+
+          <div class="card-body">
+            <h3 class="card-title">캘린더</h3>
+          </div>
+        </div>
+
+        <div class="back">
+          캘린더 설명
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+@import url('@/assets/css/mainView.css');
+
+</style>
