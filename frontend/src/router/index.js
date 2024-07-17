a849248ea3418efd55f05cd07be419f8c9c0c235
dbqudwn011 <dbqudwn011@naver.com>
2024-07-17 17:44:30 +0900

    [#7] design: MainView 제작

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,11 @@ const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
 		{
+			path: '/',
+			name: 'main',
+			component: () => import('@/views/MainView.vue'),
+		},
+		{
 			path: '/calendar',
 			name: 'CalendarView',
 			component: () => import('@/views/CalendarView.vue'),
