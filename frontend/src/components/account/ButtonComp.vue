0500f8ad908ee1cd4599dc513cc522de77d9444f
김예훈 <rladpgns00@gmail.com>
2024-07-17 17:12:18 +0900

    [#8] design: 로그인, 회원가입, 정보 수정, 비밀번호 찾기, 비밀번호 변경 화면 관련 View, Component 추가

---
+++ frontend/src/components/account/ButtonComp.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+    type: String
+})
+</script>
+
+<template>
+    <button
+        :class="[type == 'none' ? 'button-none' : type == 'check' ? 'border-left-none background-color-BDC8CF width-20per' : 'background-color-BDC8CF height-40 border-normal mt-10']">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/account/InputComp.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ButtonComp from "@/components/account/ButtonComp.vue";
+
+defineProps({
+    label: String,
+    textLabel: String,
+    textButton: String,
+    type: String,
+    show: String,
+    withButton: String,
+})
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <label :for="label" class="">{{ textLabel }}</label>
+        <div :class="{ flex: withButton == 'true' }">
+            <input :type="type" class="border-normal width-250 height-40" :class="show" :id="label" :disabled=show />
+            <ButtonComp v-if="withButton == 'true'" type="check">{{ textButton }}</ButtonComp>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
