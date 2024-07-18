7026b4cafb37ec9c8296b30f1ac848a912b3abde
김예훈 <rladpgns00@gmail.com>
2024-07-18 10:37:22 +0900

    [#8] design: InputComp 컴포넌트의 input 태그 box-sizing을 border-box로 변경 form 태그 내부에 있는 input 태그와 button 태그의 크기가 서로 다른 문제가 있었는데, input 태그의 box-sizing을 button 태그와 통일하기 위해 border-box로 변경

--- frontend/src/components/account/InputComp.vue
+++ frontend/src/components/account/InputComp.vue
@@ -15,7 +15,7 @@ defineProps({
     <div class="flex flex-col">
         <label :for="label" class="">{{ textLabel }}</label>
         <div :class="{ flex: withButton == 'true' }">
-            <input :type="type" class="border-normal width-100 height-40px"
+            <input :type="type" class="border-normal width-100 height-40px border-box"
                 :class="{ 'disabled-color': isDisabled == 'true' }" :id="label" :disabled="isDisabled == 'true'" />
             <ButtonComp v-if="withButton == 'true'" type="check">{{ textButton }}</ButtonComp>
         </div>
