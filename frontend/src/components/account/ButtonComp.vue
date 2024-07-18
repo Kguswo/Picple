3d0f809aa8b32d016a7c20a726993ca1e1eed6c8
김예훈 <rladpgns00@gmail.com>
2024-07-18 15:40:07 +0900

    [#8] design: account.css 클래스명 및 내용 수정

--- frontend/src/components/account/ButtonComp.vue
+++ frontend/src/components/account/ButtonComp.vue
@@ -5,8 +5,7 @@ defineProps({
 </script>
 
 <template>
-    <button class="background-color-BDC8CF"
-        :class="[type == 'check' ? 'border-left-none  width-20' : 'height-40px border-normal mt-10']">
+    <button class="background-color-accept" :class="[type == 'check' ? 'button-small' : 'button-big mt-10']">
         <slot></slot>
     </button>
 </template>
--- frontend/src/views/account/LoginView.vue
+++ frontend/src/views/account/LoginView.vue
@@ -9,13 +9,13 @@ import FormComp from "@/components/account/FormComp.vue";
     <AccountMainComp title="로그인">
         <FormComp>
             <InputComp label="email" text-label="이메일" type="email" />
-            <InputComp label="password" text-label="비밀번호" type="password" class="mt-5" />
+            <InputComp label="password" text-label="비밀번호" type="password" class="mt-10" />
 
             <ButtonComp>로그인</ButtonComp>
 
-            <div class="flex justify-content-between mt-10">
-                <router-link :to="{ name: 'signup' }" class="font-size-button-none">회원가입</router-link>
-                <router-link :to="{ name: 'findPassword' }" class="font-size-button-none">비밀번호 찾기</router-link>
+            <div class="flex-justify-content-between mt-10">
+                <router-link :to="{ name: 'signup' }" class="button-none">회원가입</router-link>
+                <router-link :to="{ name: 'findPassword' }" class="button-none">비밀번호 찾기</router-link>
             </div>
         </FormComp>
     </AccountMainComp>
