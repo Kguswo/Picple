cbf0a090d173210912ad6388f4d9edbe43f48789
박성훈 <kodd1102@naver.com>
2024-07-18 15:52:12 +0900

    [#9] feat : featuring the test code of user service

--- backend/src/main/java/com/ssafy/picple/domain/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/picple/domain/user/controller/UserController.java
@@ -1,7 +1,43 @@
 package com.ssafy.picple.domain.user.controller;
 
+import com.ssafy.picple.config.baseResponse.BaseException;
+import com.ssafy.picple.config.baseResponse.BaseResponse;
+import com.ssafy.picple.domain.user.dto.response.UserListResponse;
+import com.ssafy.picple.domain.user.entity.User;
+import com.ssafy.picple.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 테스트 코드 (User 목록 불러오기 - Dto 사용)
+     * @return
+     * @throws BaseException
+     */
+    @GetMapping("/test1")
+    public BaseResponse<UserListResponse> getUserDto() throws BaseException {
+        UserListResponse userDto = new UserListResponse();
+        userDto.setUsers(userService.getUser());
+        return new BaseResponse<>(userDto);
+    }
+
+    /**
+     * 테스트 코드 (User 목록 불러오기)
+     * @return
+     * @throws BaseException
+     */
+    @GetMapping("/test2")
+    public BaseResponse<List<User>> getUser() throws BaseException {
+        return new BaseResponse<>(userService.getUser());
+    }
 }
--- backend/src/main/java/com/ssafy/picple/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/picple/domain/user/repository/UserRepository.java
@@ -4,5 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.picple.domain.user.entity.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAll();
 }
