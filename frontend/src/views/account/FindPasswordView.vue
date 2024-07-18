301332c54d5a8638d0469e0cdbe6cc06a52e9336
김예훈 <rladpgns00@gmail.com>
2024-07-18 15:40:07 +0900

    [#8] design: account.css 클래스명 및 내용 수정

--- frontend/src/views/account/FindPasswordView.vue
+++ frontend/src/views/account/FindPasswordView.vue
@@ -8,10 +8,9 @@ import FormComp from "@/components/account/FormComp.vue";
 <template>
     <AccountMainComp title="비밀번호 찾기">
         <FormComp>
-            <InputComp label="password" text-label="이메일" text-button="인증" type="email" with-button="true"
-                class="mt-5" />
-            <InputComp label="cert-number" text-label="인증번호" text-button="확인" type="text" with-button="true"
-                class="mt-5" />
+            <InputComp label="password" text-label="이메일" text-button="인증" type="email" display="flex" />
+            <InputComp label="cert-number" text-label="인증번호" text-button="확인" type="text" display="flex"
+                class="mt-10" />
 
             <ButtonComp>다음</ButtonComp>
         </FormComp>
