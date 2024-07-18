df8007b49a6d752cac7414e0264ed29e4feaba89
JinSeok <spdlqjanf@naver.com>
2024-07-18 11:11:47 +0900

    [#10] fix: 디자인 및 css 에러 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,27 @@
 <script setup>
 import { RouterView, RouterLink } from "vue-router";
-
 </script>
 
 <template>
-  <div class="main">
-    <RouterView/>
-    <!-- <router-link :to="{ name: '' }"></router-link> -->
-  </div>
+    <div class="main">
+        <RouterView />
+        <!-- <router-link :to="{ name: '' }"></router-link> -->
+    </div>
 </template>
 
 <style scoped>
 .main {
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  background-image: url('@/assets/img/app/backgroundImg.png');
-  background-repeat: no-repeat;
-  background-size: cover;
-  opacity: 1;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    background-image: url("@/assets/img/app/backgroundImg.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    opacity: 1;
 
-  /* display */
-  /* display: flex; */
-  justify-content: center;
-  align-items: center;
+    /* display */
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
