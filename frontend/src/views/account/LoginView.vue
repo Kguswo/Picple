660a9ec1e6669d4d6d25b09a3d014ed7044db681
김예훈 <rladpgns00@gmail.com>
2024-07-18 10:32:07 +0900

    [#8] refactor: account form 컴포넌트화 account 관련 페이지에 들어가는 form이 중복되어 컴포넌트화함

--- frontend/src/views/account/LoginView.vue
+++ frontend/src/views/account/LoginView.vue
@@ -2,11 +2,12 @@
 import AccountMainComp from "@/components/account/AccountMainComp.vue";
 import InputComp from "@/components/account/InputComp.vue";
 import ButtonComp from "@/components/account/ButtonComp.vue";
+import FormComp from "@/components/account/FormComp.vue";
 </script>
 
 <template>
     <AccountMainComp title="로그인">
-        <form class="flex flex-col width-250px">
+        <FormComp>
             <InputComp label="email" text-label="이메일" type="email" />
             <InputComp label="password" text-label="비밀번호" type="password" class="mt-5" />
 
@@ -16,7 +17,7 @@ import ButtonComp from "@/components/account/ButtonComp.vue";
                 <ButtonComp type="none">회원가입</ButtonComp>
                 <ButtonComp type="none">비밀번호 찾기</ButtonComp>
             </div>
-        </form>
+        </FormComp>
     </AccountMainComp>
 </template>
 
