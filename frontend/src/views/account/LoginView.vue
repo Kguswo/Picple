b918db44ec3b9f8fcdb5a1ae9be3d2e8b03a7e90
김예훈 <rladpgns00@gmail.com>
2024-07-18 14:30:09 +0900

    [#8] refactor: 계정 관련 페이지에 Page 컴포넌트 적용 모든 계정 관련 페이지에 공통적으로 들어가는 Page, WhiteBoardComp 컴포넌트를 AccountMainComp 컴포넌트로 이동

--- frontend/src/views/account/LoginView.vue
+++ frontend/src/views/account/LoginView.vue
@@ -3,25 +3,22 @@ import AccountMainComp from "@/components/account/AccountMainComp.vue";
 import InputComp from "@/components/account/InputComp.vue";
 import ButtonComp from "@/components/account/ButtonComp.vue";
 import FormComp from "@/components/account/FormComp.vue";
-import WhiteBoardComp from "@/components/common/WhiteBoardComp.vue";
 </script>
 
 <template>
-    <WhiteBoardComp>
-        <AccountMainComp title="로그인">
-            <FormComp>
-                <InputComp label="email" text-label="이메일" type="email" />
-                <InputComp label="password" text-label="비밀번호" type="password" class="mt-5" />
+    <AccountMainComp title="로그인">
+        <FormComp>
+            <InputComp label="email" text-label="이메일" type="email" />
+            <InputComp label="password" text-label="비밀번호" type="password" class="mt-5" />
 
-                <ButtonComp>로그인</ButtonComp>
+            <ButtonComp>로그인</ButtonComp>
 
-                <div class="flex justify-content-between mt-10">
-                    <router-link :to="{ name: 'signup' }" class="font-size-button-none">회원가입</router-link>
-                    <router-link :to="{ name: 'findPassword' }" class="font-size-button-none">비밀번호 찾기</router-link>
-                </div>
-            </FormComp>
-        </AccountMainComp>
-    </WhiteBoardComp>
+            <div class="flex justify-content-between mt-10">
+                <router-link :to="{ name: 'signup' }" class="font-size-button-none">회원가입</router-link>
+                <router-link :to="{ name: 'findPassword' }" class="font-size-button-none">비밀번호 찾기</router-link>
+            </div>
+        </FormComp>
+    </AccountMainComp>
 </template>
 
 <style scoped></style>
