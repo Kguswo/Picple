20ede00bbf5d4593c5d1c09a464439c4c8d0ad29
염규영 <yeomky98@naver.com>
2024-07-19 17:10:18 +0900

    [#6] style: format import statements

--- backend/src/main/java/com/ssafy/picple/domain/photo/entity/Photo.java
+++ backend/src/main/java/com/ssafy/picple/domain/photo/entity/Photo.java
@@ -1,13 +1,18 @@
 package com.ssafy.picple.domain.photo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "photo")
 @Getter
+@Table(name = "photo")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Photo {
 
@@ -16,13 +21,13 @@ public class Photo {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-    @Column(nullable = false, length = 2083)
-    private String photoUrl;
+	@Column(nullable = false, length = 2083)
+	private String photoUrl;
 
-    @Column(nullable = false)
-    private boolean isShared = false;
+	@Column(nullable = false)
+	private boolean isShared = false;
 
-    @Column(nullable = false)
-    private boolean isDeleted;
+	@Column(nullable = false)
+	private boolean isDeleted;
 
 }
