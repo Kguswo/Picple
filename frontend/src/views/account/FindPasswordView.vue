2b80bf41306e0815a6dc011a9bf371c0a1658c6b
김예훈 <rladpgns00@gmail.com>
2024-07-19 10:01:14 +0900

    [#15] rename: Account 관련 파일 및 class 이름을 Form으로 변경 Account 관련 페이지에서 사용하던 형식을 부스 참여 페이지에서도 동일하게 사용하기 때문에 Account가 아닌 Form으로 변경

--- frontend/src/views/account/FindPasswordView.vue
+++ frontend/src/views/account/FindPasswordView.vue
@@ -1,15 +1,15 @@
 <script setup>
-import AccountMainComp from "@/components/account/AccountMainComp.vue";
+import FormComp from "@/components/FormComp.vue";
 </script>
 
 <template>
-    <AccountMainComp title="비밀번호 찾기">
-        <form class="account-form">
+    <FormComp title="비밀번호 찾기">
+        <form class="form-content">
             <div class="flex-col">
                 <label>이메일</label>
                 <div class="flex">
                     <input type="email" class="input-big" />
-                    <button class="button-small">인증</button>
+                    <button type="button" class="button-small">인증</button>
                 </div>
             </div>
 
@@ -17,13 +17,13 @@ import AccountMainComp from "@/components/account/AccountMainComp.vue";
                 <label>인증번호</label>
                 <div class="flex">
                     <input type="text" class="input-big" />
-                    <button class="button-small">확인</button>
+                    <button type="button" class="button-small">확인</button>
                 </div>
             </div>
 
             <button class="button-big mt-10">다음</button>
         </form>
-    </AccountMainComp>
+    </FormComp>
 </template>
 
 <style scoped></style>
