2de99ea4eb87dbddaff338684fae5527a0256e3c
김예훈 <rladpgns00@gmail.com>
2024-07-22 23:59:54 +0900

    [#23] refactor: 로그인 페이지 코드 리팩터링

---
+++ frontend/src/components/form/FormInputComp.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  params: Object,
+});
+
+const inputField = ref(null);
+
+const focusInput = () => {
+  inputField.value.focus();
+};
+</script>
+
+<template>
+  <div class="input-container" @click="focusInput">
+    <input
+      :type="params.type"
+      v-model="params.value"
+      ref="inputField"
+      class="form-input"
+      :class="{ 'has-content': params.value }"
+    />
+    <label class="form-label">{{ params.label }}</label>
+  </div>
+</template>
+
+<style scoped></style>
