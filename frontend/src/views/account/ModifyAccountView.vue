6bf81ea67a90c71f842116e768f02e1f93293e47
김예훈 <rladpgns00@gmail.com>
2024-07-23 01:06:52 +0900

    [#23] refactor: 정보 수정 페이지 코드 리팩터링

--- frontend/src/views/account/ModifyAccountView.vue
+++ frontend/src/views/account/ModifyAccountView.vue
@@ -1,57 +1,59 @@
 <script setup>
 import FormComp from "@/components/form/FormComp.vue";
 import FormMessageComp from "@/components/form/FormMessageComp.vue";
+import FormInputComp from "@/components/form/FormInputComp.vue";
+import FormButtonComp from "@/components/form/FormButtonComp.vue";
 import { useRouter } from "vue-router";
-import { useFormInput, enableFocus } from "@/stores/form";
-import { onMounted } from "vue";
 import valdiate from "@/stores/validation";
-
-onMounted(() => {
-    enableFocus();
-})
+import { ref } from "vue";
 
 const router = useRouter();
 
-const { objects, handleFocus, handleBlur } = useFormInput(['nickname']);
-const { nickname } = objects;
-const { message, validateModifyAccount, checkNicknameDup } = valdiate();
+const { message, checkNickname, checkNicknameDup } = valdiate();
 
-function modifyPassword() {
-    router.push({ name: 'modifyPassword', params: { path: 'modify' } });
-}
+const nickname = ref({ type: "text", label: "닉네임", value: "" });
+
+const routerModifyPassword = () => {
+  router.push({ name: "modifyPassword", params: { path: "modify" } });
+};
+
+const modify = () => {
+  if (!checkNickname(nickname.value.value)) {
+    return;
+  }
+  // todo: 정보 수정
+};
 </script>
 
 <template>
-    <FormComp title="정보 수정">
-        <form class="form-content">
-            <div class="input-container background-color-disabled">
-                <input type="text" class="form-input has-content background-color-disabled" disabled />
-                <label class="form-label">이메일</label>
-            </div>
+  <FormComp title="정보 수정">
+    <form class="form-content">
+      <div class="input-container background-color-disabled">
+        <input type="text" class="form-input has-content background-color-disabled" disabled />
+        <label class="form-label">이메일</label>
+      </div>
 
-            <div class="input-container mt-10">
-                <input type=" text" @focus="handleFocus(nickname)" @blur="handleBlur(nickname)" v-model="nickname.value"
-                    class="form-input" :class="{ 'has-content': nickname.value }" />
-                <label class="form-label">닉네임</label>
-                <button type="button" class="form-button-small" @click="checkNicknameDup(nickname)">중복</button>
-            </div>
+      <FormInputComp :params="nickname" class="mt-10">
+        <FormButtonComp size="small" @click-button="checkNicknameDup(nickname.value)"
+          >중복</FormButtonComp
+        >
+      </FormInputComp>
 
-            <div class="input-container background-color-disabled mt-10">
-                <input type="password" class="form-input has-content background-color-disabled" autoComplete="off"
-                    disabled />
-                <label class="form-label">비밀번호</label>
-                <button type="button" class="form-button-small" @click="modifyPassword">변경</button>
-            </div>
+      <div class="input-container background-color-disabled mt-10">
+        <input type="password" class="form-input has-content background-color-disabled" disabled />
+        <label class="form-label">비밀번호</label>
+        <button type="button" class="form-button-small" @click="routerModifyPassword">변경</button>
+      </div>
 
-            <FormMessageComp :message="message" />
+      <FormMessageComp :message="message" />
 
-            <button type="button" class="form-button-big mt-20" @click="validateModifyAccount(nickname)">저장</button>
+      <FormButtonComp size="big" @click-button="modify">저장</FormButtonComp>
 
-            <div class="text-align-right mt-10">
-                <button type="button" class="form-button-none">회원탈퇴</button>
-            </div>
-        </form>
-    </FormComp>
+      <div class="text-align-right mt-10">
+        <button type="button" class="form-button-none">회원탈퇴</button>
+      </div>
+    </form>
+  </FormComp>
 </template>
 
 <style scoped></style>
