0ab1d5825a13b87341f39d03808069ed70ce5cd5
dbqudwn011 <dbqudwn011@naver.com>
2024-07-22 13:10:59 +0900

    [#19] design: createBoothView 수정 & Component줄이고 View를 사용

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,68 +1,60 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: "/",
-			name: "main",
-			component: () => import("@/views/MainView.vue"),
-		},
-		{
-			path: "/calendar",
-			name: "CalendarView",
-			component: () => import("@/views/CalendarView.vue"),
-		},
-		{
-			path: "/login",
-			name: "login",
-			component: () => import("@/views/account/LoginView.vue"),
-		},
-		{
-			path: "/signup",
-			name: "signup",
-			component: () => import("@/views/account/SignupView.vue"),
-		},
-		{
-			path: "/modifyAccount",
-			name: "modifyAccount",
-			component: () => import("@/views/account/ModifyAccountView.vue"),
-		},
-		{
-			path: "/modifyPassword/:path",
-			name: "modifyPassword",
-			component: () => import("@/views/account/ModifyPasswordView.vue"),
-		},
-		{
-			path: "/findPassword",
-			name: "findPassword",
-			component: () => import("@/views/account/FindPasswordView.vue"),
-		},
-		{
-
-			path: "/board",
-			name: "board",
-			component: () => import("@/views/BoardView.vue"),
-		},
-		{
-			path: "/booth",
-			name: "booth",
-			component: () => import("@/views/booth/BoothView.vue"),
-			children: [
-					{
-				  		path: "",
-				  		name: "boothCreate",
-				  		component: () => import("@/components/booth/CreateBoothComp.vue"),
-					},
-				]
-		},
-		{
-			path: "/boothCode",
-			name: "boothCode",
-			component: () => import("@/views/booth/BoothCodeView.vue"),
-		},
-
-	],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "main",
+            component: () => import("@/views/MainView.vue"),
+        },
+        {
+            path: "/calendar",
+            name: "CalendarView",
+            component: () => import("@/views/CalendarView.vue"),
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: () => import("@/views/account/LoginView.vue"),
+        },
+        {
+            path: "/signup",
+            name: "signup",
+            component: () => import("@/views/account/SignupView.vue"),
+        },
+        {
+            path: "/modifyAccount",
+            name: "modifyAccount",
+            component: () => import("@/views/account/ModifyAccountView.vue"),
+        },
+        {
+            path: "/modifyPassword/:path",
+            name: "modifyPassword",
+            component: () => import("@/views/account/ModifyPasswordView.vue"),
+        },
+        {
+            path: "/findPassword",
+            name: "findPassword",
+            component: () => import("@/views/account/FindPasswordView.vue"),
+        },
+        {
+            path: "/board",
+            name: "board",
+            component: () => import("@/views/BoardView.vue"),
+        },
+        {
+            path: "/booth",
+            name: "booth",
+            component: () => import("@/views/booth/BoothView.vue"),
+            children: [],
+        },
+        {
+            path: "/boothCode",
+            name: "boothCode",
+            component: () => import("@/views/booth/BoothCodeView.vue"),
+        },
+    ],
 });
 
 export default router;
