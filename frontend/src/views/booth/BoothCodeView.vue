e5517a3be06cb788cb0c4086261ea741702b08d4
김예훈 <rladpgns00@gmail.com>
2024-07-23 01:29:12 +0900

    [#23] refactor: 부스 참여 페이지 코드 리팩터링

--- frontend/src/views/booth/BoothCodeView.vue
+++ frontend/src/views/booth/BoothCodeView.vue
@@ -1,50 +1,42 @@
 <script setup>
-import FormComp from '@/components/form/FormMainComp.vue';
-import FormMessageComp from '@/components/form/MessageComp.vue';
+import FormComp from '@/components/form/FormComp.vue';
+import FormMessageComp from '@/components/form/FormMessageComp.vue';
+import FormInputComp from '@/components/form/FormInputComp.vue';
+import FormButtonComp from '@/components/form/FormButtonComp.vue';
 import { useRouter } from 'vue-router';
-import { onMounted } from 'vue';
-import { useFormInput, enableFocus } from '@/stores/form';
 import validate from '@/stores/validation';
-
-onMounted(() => {
-	enableFocus();
-});
+import { ref } from 'vue';
 
 const router = useRouter();
 
-const { objects, handleFocus, handleBlur } = useFormInput(['boothCode']);
-const { boothCode } = objects;
-const { message, validateBoothCode } = validate();
+const { message, checkBoothCode } = validate();
 
-function cancel() {
+const boothCode = ref({ type: 'text', label: '부스 코드', value: '' });
+
+const join = () => {
+	if (!checkBoothCode(boothCode.value.value)) {
+		return;
+	}
+	// todo: 부스 참여
+};
+
+const cancel = () => {
 	router.push({ name: 'main' });
-}
+};
 </script>
 
 <template>
 	<FormComp title="부스 참여">
 		<form class="form-content">
-			<div class="input-container">
-				<input
-					type="text"
-					@focus="handleFocus(boothCode)"
-					@blur="handleBlur(boothCode)"
-					v-model="boothCode.value"
-					class="form-input"
-					:class="{ 'has-content': boothCode.value }"
-				/>
-				<label class="form-label">부스 코드</label>
-			</div>
+			<FormInputComp :params="boothCode" />
 
 			<FormMessageComp :message="message" />
 
-			<button
-				type="button"
-				class="form-button-big mt-20"
-				@click="validateBoothCode(boothCode)"
+			<FormButtonComp
+				size="big"
+				@click-button="join"
+				>확인</FormButtonComp
 			>
-				확인
-			</button>
 			<button
 				type="button"
 				class="form-button-big form-button-cancel mt-10"
