d10bf9e0e0b485fe3eb7b7e4f481b923c887cfa5
김예훈 <rladpgns00@gmail.com>
2024-07-23 00:25:40 +0900

    [#23] refactor: 이메일 인증 페이지 코드 리팩터링

--- frontend/src/components/form/FormEmailComp.vue
+++ frontend/src/components/form/FormEmailComp.vue
@@ -1,38 +1,40 @@
 <script setup>
 import FormMessageComp from "@/components/form/FormMessageComp.vue";
-import { useFormInput, enableFocus } from "@/stores/form";
+import FormInputComp from "@/components/form/FormInputComp.vue";
+import FormButtonComp from "@/components/form/FormButtonComp.vue";
 import validate from "@/stores/validation";
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 
-onMounted(() => {
-    enableFocus();
-})
+const email = ref({ type: "text", label: "이메일", value: "" });
+const certNumber = ref({ type: "text", label: "인증번호", value: "" });
 
-const { objects, handleFocus, handleBlur } = useFormInput(['email', 'certNum', 'nickname', 'password', 'passwordCheck']);
-const { email, certNum } = objects;
-const { message, validateEmail, sendCertNum } = validate();
+const { message, validateEmail } = validate();
+
+const sendCertNumber = () => {
+  if (!validateEmail(email.value.value)) {
+    return;
+  }
+  // todo: 인증번호 전송
+};
+
+const certify = () => {
+  // todo: 인증번호 일치 여부 검사
+  // todo: 이미 등록된 이메일 여부 검사
+};
 </script>
 
 <template>
-    <form class="form-content">
-        <div class="input-container">
-            <input type="text" @focus="handleFocus(email)" @blur="handleBlur(email)" v-model="email.value"
-                class="form-input" :class="{ 'has-content': email.value }" />
-            <label class="form-label">이메일</label>
-            <button type="button" class="form-button-small" @click="sendCertNum(email)">인증</button>
-        </div>
+  <form class="form-content">
+    <FormInputComp :params="email">
+      <FormButtonComp size="small" @click-button="sendCertNumber">인증</FormButtonComp>
+    </FormInputComp>
 
-        <div class="input-container mt-10">
-            <input type="text" @focus="handleFocus(certNum)" @blur="handleBlur(certNum)" v-model="certNum.value"
-                class="form-input" :class="{ 'has-content': certNum.value }" />
-            <label class="form-label">인증번호</label>
-            <!-- <button class="form-button-small">확인</button> -->
-        </div>
+    <FormInputComp :params="certNumber" class="mt-10" />
 
-        <FormMessageComp :message="message" />
+    <FormMessageComp :message="message" />
 
-        <button type="button" class="form-button-big mt-20" @click="validateEmail(email, certNum)">다음</button>
-    </form>
+    <FormButtonComp size="big" @click-button="certify">다음</FormButtonComp>
+  </form>
 </template>
 
 <style scoped></style>
