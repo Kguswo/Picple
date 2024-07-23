a70ff41b0975ac7052348c3db202a87668d284ff
김예훈 <rladpgns00@gmail.com>
2024-07-23 13:10:41 +0900

    [#24] feat: form에서 input 필드별로 최대 길이 제한

--- frontend/src/components/form/FormInputComp.vue
+++ frontend/src/components/form/FormInputComp.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from "vue";
 
-defineProps({
+const props = defineProps({
   params: Object,
 });
 
 const inputField = ref(null);
+const maxLength = props.params.label === '이메일' ? 254 : props.params.label === '닉네임' ? 8 : props.params.label.includes('비밀번호') ? 16 : 10;
 
 const focusInput = () => {
   inputField.value.focus();
@@ -19,7 +20,7 @@ defineExpose({
 <template>
   <div class="input-container" @click="focusInput">
     <input :type="params.type" v-model="params.value" ref="inputField" class="form-input"
-      :class="{ 'has-content': params.value }" />
+      :class="{ 'has-content': params.value }" :maxlength="maxLength" />
     <label class="form-label">{{ params.label }}</label>
     <slot></slot>
   </div>
