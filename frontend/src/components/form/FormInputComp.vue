143e25bfaa23cfe61fc9f520e433e3d39a244588
김예훈 <rladpgns00@gmail.com>
2024-07-23 14:58:49 +0900

    [#24] feat: form 입력 시 내용을 지우는 취소 버튼 추가

--- frontend/src/components/form/FormInputComp.vue
+++ frontend/src/components/form/FormInputComp.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from "vue";
+import FormIconCancelComp from "@/components/form/FormIconCancelComp.vue";
+import FormIconVisibilityComp from "@/components/form/FormIconVisibilityComp.vue";
 
 const props = defineProps({
   params: Object,
@@ -12,6 +14,14 @@ const focusInput = () => {
   inputField.value.focus();
 };
 
+const toggleVisibility = (newType) => {
+  props.params.type = newType;
+}
+
+const cancelInput = () => {
+  props.params.value = "";
+}
+
 defineExpose({
   focusInput,
 })
@@ -22,8 +32,16 @@ defineExpose({
     <input :type="params.type" v-model="params.value" ref="inputField" class="form-input"
       :class="{ 'has-content': params.value }" :maxlength="maxLength" />
     <label class="form-label">{{ params.label }}</label>
-    <slot></slot>
+
+    <div class="form-input-etc">
+      <FormIconCancelComp v-if="params.value" class="form-icon-cancel" @click="cancelInput" />
+      <FormIconVisibilityComp v-if="params.label.includes('비밀번호') && params.value" class="form-icon-visibility"
+        :type="params.type" @toggle-visibility="toggleVisibility" />
+      <slot></slot>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
