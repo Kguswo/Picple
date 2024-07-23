d7a4bb8be652d2ddca1762267031109931bb19a9
염규영 <yeomky98@naver.com>
2024-07-23 13:32:07 +0900

    [#6] refactor: remove unnecessary annotation

--- backend/src/main/java/com/ssafy/picple/domain/background/repository/BackgroundRepository.java
+++ backend/src/main/java/com/ssafy/picple/domain/background/repository/BackgroundRepository.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import com.ssafy.picple.domain.background.entity.Background;
 
-@Repository
 public interface BackgroundRepository extends JpaRepository<Background, Long> {
 
 	// 해당 유저의 삭제되지 않은 배경 사진만
