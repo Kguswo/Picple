bf96162bfc1407f201b588f6ad06845edc245da5
김예훈 <rladpgns00@gmail.com>
2024-07-23 16:52:50 +0900

    [#24] feat: form에서 각 input 필드 아래에 메시지 출력

--- frontend/src/components/form/FormInputComp.vue
+++ frontend/src/components/form/FormInputComp.vue
@@ -2,11 +2,14 @@
 import { ref } from "vue";
 import FormIconCancelComp from "@/components/form/FormIconCancelComp.vue";
 import FormIconVisibilityComp from "@/components/form/FormIconVisibilityComp.vue";
+import FormMessageComp from "@/components/form/FormMessageComp.vue";
 
 const props = defineProps({
   params: Object,
   isSend: Boolean,
 });
+
+const message = ref(null)
 
 const inputField = ref(null);
 const maxLength = props.params.label === '이메일' ? 254 : props.params.label === '닉네임' ? 8 : props.params.label.includes('비밀번호') ? 16 : 10;
@@ -25,22 +28,28 @@ const cancelInput = () => {
 
 defineExpose({
   focusInput,
+  message
 })
 </script>
 
 <template>
-  <div class="input-container" :class="{ 'background-color-disabled': isSend === true }" @click="focusInput">
-    <input :type="params.type" v-model="params.value" ref="inputField" class="form-input"
-      :class="{ 'has-content': params.value, 'background-color-disabled': isSend === true }" :maxlength="maxLength"
-      :disabled="isSend === true" />
-    <label class="form-label">{{ params.label }}</label>
+  <div class="form-input-conatiner">
+    <div class="input-container"
+      :class="{ 'input-container-selected': !message || !message.isError, 'input-container-error': message && message.isError, 'background-color-disabled': isSend === true }"
+      @click="focusInput">
+      <input :type="params.type" v-model="params.value" ref="inputField" class="form-input"
+        :class="{ 'has-content': params.value, 'background-color-disabled': isSend === true }" :maxlength="maxLength"
+        :disabled="isSend === true" />
+      <label class="form-label">{{ params.label }}</label>
 
-    <div class="form-input-etc">
-      <FormIconCancelComp v-if="params.value" class="form-icon-cancel" @click="cancelInput" />
-      <FormIconVisibilityComp v-if="params.label.includes('비밀번호') && params.value" class="form-icon-visibility"
-        :type="params.type" @toggle-visibility="toggleVisibility" />
-      <slot></slot>
+      <div class="form-input-etc">
+        <FormIconCancelComp v-if="params.value" class="form-icon-cancel" @click="cancelInput" />
+        <FormIconVisibilityComp v-if="params.label.includes('비밀번호') && params.value" class="form-icon-visibility"
+          :type="params.type" @toggle-visibility="toggleVisibility" />
+        <slot></slot>
+      </div>
     </div>
+    <FormMessageComp v-if="message" :message="message" />
   </div>
 </template>
 
