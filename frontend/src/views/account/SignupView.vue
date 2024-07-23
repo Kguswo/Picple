e58fa2754443b219f77570b37d396d3e122788b2
김예훈 <rladpgns00@gmail.com>
2024-07-23 12:34:23 +0900

    [#24] feat: form에서 엔터키 누를 시 form 전송과 동일한 효과 적용

--- frontend/src/views/account/SignupView.vue
+++ frontend/src/views/account/SignupView.vue
@@ -11,32 +11,43 @@ const { message, validatePassword, confirmPassword, checkNickname, checkNickname
 const nickname = ref({ type: "text", label: "닉네임", value: "" });
 const password = ref({ type: "password", label: "비밀번호", value: "" });
 const passwordConfirm = ref({ type: "password", label: "비밀번호 확인", value: "" });
+const nicknameField = ref(null);
+const passwordField = ref(null);
+const passwordConfirmField = ref(null);
 
 const signup = () => {
   if (!checkNickname(nickname.value.value)) {
+    nicknameField.value.focusInput();
     return;
   }
   if (!validatePassword(password.value.value)) {
+    passwordField.value.focusInput();
     return;
   }
   if (!confirmPassword(password.value.value, passwordConfirm.value.value)) {
+    passwordConfirmField.value.focusInput();
     return;
   }
   // todo: 회원가입
 };
+
+const checkDuplicate = () => {
+  if (!checkNicknameDup(nickname.value.value)) {
+    return;
+  }
+}
 </script>
 
 <template>
   <FormComp title="회원가입">
-    <form class="form-content">
-      <FormInputComp :params="nickname">
-        <FormButtonComp size="small" @click-button="checkNicknameDup(nickname.value)"
-          >중복</FormButtonComp
-        >
+    <form class="form-content" @keyup.enter="signup">
+      <FormInputComp :params="nickname" ref="nicknameField">
+        <FormButtonComp size="small" @click-button="checkDuplicate">중복
+        </FormButtonComp>
       </FormInputComp>
 
-      <FormInputComp :params="password" class="mt-10" />
-      <FormInputComp :params="passwordConfirm" class="mt-10" />
+      <FormInputComp :params="password" ref="passwordField" class="mt-10" />
+      <FormInputComp :params="passwordConfirm" ref="passwordConfirmField" class="mt-10" />
 
       <FormMessageComp :message="message" />
 
