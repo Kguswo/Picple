1a190817184785b9ff50ac000d52333440f5ee11
Kguswo <khj990322@gmail.com>
2024-07-23 14:08:18 +0900

    [#22] feat: Update PhotoController, S3 Test Sucess

--- backend/src/main/java/com/ssafy/picple/domain/photo/controller/PhotoController.java
+++ backend/src/main/java/com/ssafy/picple/domain/photo/controller/PhotoController.java
@@ -1,31 +1,37 @@
 package com.ssafy.picple.domain.photo.controller;
 
-import com.ssafy.picple.AwsS3.S3FileUploadService;
-import com.ssafy.picple.config.baseResponse.BaseResponseStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.picple.AwsS3.S3FileUploadService;
 import com.ssafy.picple.config.baseResponse.BaseResponse;
+import com.ssafy.picple.config.baseResponse.BaseResponseStatus;
 import com.ssafy.picple.domain.photo.entity.Photo;
 import com.ssafy.picple.domain.photo.service.PhotoService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/photo")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class PhotoController {
 
 	private final PhotoService photoService;
 	private final S3FileUploadService s3FileUploadService;
 
-	@PostMapping("")
-	public BaseResponse<?> savePhoto(@RequestBody Photo photo, MultipartFile file) {
+	@PostMapping(value = "", consumes = "multipart/form-data")
+	public BaseResponse<?> savePhoto(@RequestPart("photo") @Valid Photo photo,
+			@RequestPart("file") MultipartFile file) {
 		try {
+			System.out.println("사진저장 전");
 			String photoUrl = s3FileUploadService.uploadFile(file);
+			System.out.println("사진저장 후");
 
 			Photo newPhoto = Photo.builder()
 					.photoUrl(photoUrl)
