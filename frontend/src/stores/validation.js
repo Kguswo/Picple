8dbf743c51cbafd5fe37b5a0f7f2c1d33605d0e0
김예훈 <rladpgns00@gmail.com>
2024-07-24 14:45:12 +0900

    [#24] refactor: 로그인 페이지 유효성 검사 코드  리팩터링

--- frontend/src/stores/validation.js
+++ frontend/src/stores/validation.js
@@ -1,108 +1,98 @@
-import { ref } from "vue";
-
 const patternNickname = /^[가-힣a-zA-Z0-9]{2,8}$/;
 const patternEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+$/;
 const patternPassword = /^(?=.*[a-zA-Z])(?=.*[!@#$%^&*?_])(?=.*[0-9]).{8,16}$/;
 const patternWhiteSpace = /\s/;
 
-const validate = () => {
-	const message = ref({ text: "", isError: true });
-	const checkedNickname = ref("");
+const validateEmailPattern = (email) => {
+	if (!patternEmail.test(email) || patternWhiteSpace.test(email)) {
+		return setFormMessage("이메일 형식이 올바르지 않습니다.", true);
+	}
+	return setFormMessage("", false);
+};
 
-	const validateEmail = (email) => {
-		if (!patternEmail.test(email) || patternWhiteSpace.test(email)) {
-			return setFormMessage("이메일 형식이 올바르지 않습니다.", true);
-		}
-		return setFormMessage("", false);
-	};
+const validatePasswordPattern = (password) => {
+	if (!patternPassword.test(password) || patternWhiteSpace.test(password)) {
+		return setFormMessage(
+			"비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자만 가능합니다.",
+			true
+		);
+	}
+	return setFormMessage("", false);
+};
 
-	const validatePassword = (password) => {
-		if (
-			!patternPassword.test(password) ||
-			patternWhiteSpace.test(password)
-		) {
-			return setFormMessage(
-				"비밀번호는 8~16자의 영문 대/소문자, 숫자, 특수문자만 가능합니다.",
-				true
-			);
-		}
-		return setFormMessage("", false);
-	};
+const validateNicknamePattern = (nickname) => {
+	if (!patternNickname.test(nickname) || patternWhiteSpace.test(nickname)) {
+		return setFormMessage(
+			"닉네임은 2~8자의 한글, 영문, 숫자만 가능합니다.",
+			true
+		);
+	}
+	return setFormMessage("", false);
+};
 
-	const validateNickname = (nickname) => {
-		if (
-			!patternNickname.test(nickname) ||
-			patternWhiteSpace.test(nickname)
-		) {
-			return etFormMessage(
-				"닉네임은 2~8자의 한글, 영문, 숫자만 가능합니다.",
-				true
-			);
-		}
-		return setFormMessage("", false);
-	};
+const validateCertNumber = (certNumber) => {
+	// todo: 전송한 인증번호와 입력한 인증번호가 일치하는지 검사
+	return setFormMessage("", false);
+};
 
-	const confirmPassword = (password, passwordConfirm) => {
-		if (!passwordConfirm || password !== passwordConfirm) {
-			return setFormMessage("비밀번호가 일치하지 않습니다.", true);
-		}
-		return setFormMessage("", false);
-	};
+const validatePasswordConfirm = (password, passwordConfirm) => {
+	if (!passwordConfirm || password !== passwordConfirm) {
+		return setFormMessage("비밀번호가 일치하지 않습니다.", true);
+	}
+	return setFormMessage("", false);
+};
 
-	const checkCertEmail = (isSend) => {
-		if (!isSend) {
-			return setFormMessage("이메일 인증이 필요합니다.", true);
-		}
-		return setFormMessage("", false);
-	};
+const validateNicknameDup = (nickname) => {
+	// todo: 닉네임 중복 여부 검사
+	return setFormMessage("사용 가능한 닉네임입니다.", false);
+};
 
-	const checkNickname = (nickname) => {
-		if (!checkedNickname.value || checkedNickname.value !== nickname) {
-			return setFormMessage("닉네임 중복 확인이 필요합니다.", true);
-		}
-		return setFormMessage("", false);
-	};
+const validateCurrentPassword = (currentPassword) => {
+	if (!currentPassword) {
+		return setFormMessage("비밀번호를 틀렸습니다.");
+	}
+	// todo: 현재 비밀번호 일치 여부 검사
+	return setFormMessage("", false);
+};
 
-	const checkNicknameDup = (nickname) => {
-		const nicknameMessage = validateNickname(nickname);
-		if (nicknameMessage.isError) {
-			return nicknameMessage;
-		}
-		// todo: 닉네임 중복 여부 검사
-		checkedNickname.value = nickname;
-		return setFormMessage("사용 가능한 닉네임입니다.", false);
-	};
+const validateBoothCode = (boothCode) => {
+	if (!boothCode) {
+		return setFormMessage("부스 코드를 입력하세요.", true);
+	}
+	// todo: 부스 코드 일치 여부 검사
+	return setFormMessage("", false);
+};
 
-	const checkOldPassword = (oldPassword) => {
-		// todo: 현재 비밀번호 일치 여부 검사
-		return setFormMessage("", false);
-	};
+const validateLogin = (fields, email, password) => {
+	const emailMessage = validateEmailPattern(email);
+	const passwordMessage = validatePasswordPattern(password);
+	const messages = [emailMessage, passwordMessage];
 
-	const checkBoothCode = (boothCode) => {
-		if (!boothCode) {
-			return setFormMessage("부스 코드를 입력하세요.", true);
-		}
-		// todo: 부스 코드 일치 여부 검사
-		return setFormMessage("", false);
-	};
+	const isFail = printMessageAndFocus(fields, messages);
+	if (isFail) {
+		return false;
+	}
+	// todo: 계정 일치 검사
+	return true;
+};
 
-	const setFormMessage = (text, isError) => {
-		return { text, isError };
-	};
+const validateEmailBeforeSend = (fields, email) => {
+	const emailMessage = validateEmailPattern(email);
+	const messages = [emailMessage];
 
-	return {
-		message,
-		validateEmail,
-		validatePassword,
-		validateNickname,
-		confirmPassword,
-		checkCertEmail,
-		checkNickname,
-		checkNicknameDup,
-		checkOldPassword,
-		checkBoothCode,
-		setFormMessage,
-	};
+	const isFail = printMessageAndFocus(fields, messages);
+	if (isFail) {
+		return false;
+	}
+	fields[0].message = setFormMessage("인증번호를 전송하였습니다.", false);
+	return true;
+};
+
+const validateEmailCert = (fields, certNumber, isSend) => {
+	if (!isSend) {
+		return setFormMessage("이메일 인증이 필요합니다.", true);
+	}
+	return setFormMessage("", false);
 };
 
 const printMessageAndFocus = (fields, messages) => {
@@ -121,4 +111,8 @@ const printMessageAndFocus = (fields, messages) => {
 	return focused;
 };
 
-export { validate, printMessageAndFocus };
+const setFormMessage = (text, isError) => {
+	return { text, isError };
+};
+
+export { validateLogin, validateEmailBeforeSend };
