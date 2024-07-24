1b2b8ff14dc5c10a8bba90193714057741bc0477
김예훈 <rladpgns00@gmail.com>
2024-07-24 14:45:12 +0900

    [#24] refactor: 로그인 페이지 유효성 검사 코드  리팩터링

--- frontend/src/views/account/LoginView.vue
+++ frontend/src/views/account/LoginView.vue
@@ -2,28 +2,21 @@
 import FormComp from "@/components/form/FormComp.vue";
 import FormInputComp from "@/components/form/FormInputComp.vue";
 import FormButtonComp from "@/components/form/FormButtonComp.vue";
-import { validate, printMessageAndFocus } from "@/stores/validation";
+import { validateLogin } from "@/stores/validation";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-const { validateEmail, validatePassword } = validate();
 
-const email = ref({ type: "text", label: "이메일", value: "" });
+const email = ref({ type: "email", label: "이메일", value: "" });
 const password = ref({ type: "password", label: "비밀번호", value: "" });
 const emailField = ref(null);
 const passwordField = ref(null);
 
 const login = () => {
-  const emailMessage = validateEmail(email.value.value);
-  const passwordMessage = validatePassword(password.value.value);
-
   const fields = [emailField.value, passwordField.value];
-  const messages = [emailMessage, passwordMessage]
-
-  // todo: 계정 일치 여부 검사
-  const isFail = printMessageAndFocus(fields, messages);
-  if (isFail) {
+  const isSuccess = validateLogin(fields, email.value.value, password.value.value);
+  if (!isSuccess) {
     return;
   }
   // todo: 로그인 성공
@@ -37,8 +30,8 @@ const changeView = (viewName) => {
 <template>
   <FormComp title="로그인">
     <form class="form-content" @keyup.enter="login">
-      <FormInputComp :params="email" ref="emailField" />
-      <FormInputComp :params="password" ref="passwordField" class="mt-10" />
+      <FormInputComp :params="email" ref="emailField" name="username" />
+      <FormInputComp :params="password" ref="passwordField" class="mt-10" name="current-password" />
 
       <FormButtonComp size="big" @click="login">로그인</FormButtonComp>
 
