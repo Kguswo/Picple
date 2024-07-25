8e63272b3c982a3da72a8cf9e0c8a3eae300e8cb
염규영 <yeomky98@naver.com>
2024-07-25 18:07:55 +0900

    [#21] refactor: change method name

--- backend/src/main/java/com/ssafy/picple/domain/photouser/repository/PhotoUserRepository.java
+++ backend/src/main/java/com/ssafy/picple/domain/photouser/repository/PhotoUserRepository.java
@@ -13,11 +13,15 @@ public interface PhotoUserRepository extends JpaRepository<PhotoUser, Long> {
 
 	// 유저가 가진 모든 사진 찾기
 	@Query("SELECT pu FROM PhotoUser pu WHERE pu.user.id = :userId")
-	List<PhotoUser> findAllByUserId(@Param("userId") Long userId);
+	List<PhotoUser> findByUserId(@Param("userId") Long userId);
 
 	// 사진에 포함된 모든 유저 찾기
 	@Query("SELECT pu FROM PhotoUser pu WHERE pu.photo.id = :photoId")
-	List<PhotoUser> findAllByPhotoId(@Param("photoId") Long photoId);
+	List<PhotoUser> findByPhotoId(@Param("photoId") Long photoId);
+
+	// 유저의 특정 사진 찾기
+	@Query("SELECT pu FROM PhotoUser pu WHERE pu.photo.id = :photoId AND pu.user.id = :userId")
+	PhotoUser findByPhotoIdAndUserId(@Param("photoId") Long photoId, @Param("userId") Long userId);
 
 	// 사진의 Content 업데이트
 	@Modifying
