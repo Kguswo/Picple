c964832a195e057ff015f8211a2e26d4d4c4388f
dbqudwn011 <dbqudwn011@naver.com>
2024-07-25 15:46:54 +0900

    [#19] Design: 사진 촬영 구현 완료

--- frontend/src/views/booth/BoothShootView.vue
+++ frontend/src/views/booth/BoothShootView.vue
@@ -13,6 +13,7 @@ import { RouterView, useRouter } from "vue-router";
 import { ref, onMounted, onUnmounted, nextTick } from 'vue';
 import html2canvas from "html2canvas";
 
+//화면 전환을 위한 router
 const router = useRouter();
 
 const navigateTo = (path) => {
@@ -24,14 +25,15 @@ const videoElement = ref(null);
 let mediaStream = null;
 
 // 화면 표시에 있어 사용되는 변수
-let isMirrored = false;
-let isvideoOn = ref(true); 
-let isMicroOn = ref(true);
+let isMirrored = false; // 거울모드 여부
+let isvideoOn = ref(true);  // 비디오 ON/OFF 여부
+let isMicroOn = ref(true); // 마이크 ON/OFF 여부
+const remainPicCnt = ref(10);
 
 onMounted(async () => {
   console.log('shootView Mounted!')
   try {
-    mediaStream = await navigator.mediaDevices.getUserMedia({ video: true,audio: true });
+    mediaStream = await navigator.mediaDevices.getUserMedia({ video: true,audio: true }); // mediaStream을 열어 영상과 목소리 넣기
 
     videoElement.value.srcObject = mediaStream;
   } catch (error) {
@@ -102,47 +104,96 @@ const changeImage = (image) => {
 };
 
 //촬영 버튼
-const captureArea = ref(null); 
-const images = ref([]);
+const captureArea = ref(null); // 사진 저장 범위 ref를 통해 되어있음
+const images = ref([]); // 찍힌 사진 저장 배열
+const countdown = ref(0); 
+var cameraaudio = new Audio('/src/assets/audio/shutter.mp3'); // 카메라 찰칵 오디오 파일 js는 직접경로 필요로 @불가
 
+// 카운트다운 진행 함수
+const startCountdown = () => {
+  const countdownInterval = setInterval(() => {
+    countdown.value--;
+    if (countdown.value <= 0) {
+      clearInterval(countdownInterval);
+      Swal.close();  // 모달 닫기
+      capturePhoto(); // 카운트다운이 끝나면 사진 촬영
+    } else {
+      Swal.update({
+        html: `<p style='color:white; font-size:50px;'>${countdown.value}</h3>` // 시간이 2초 이하일 때 나타내는 것
+      });
+    }
+  }, 1000);
+};
+
+// 사진 촬영 함수
 const takePhoto = async () => {
   console.log('사진 찍기');
-  
   // 이미지가 로드되었는지 확인
   const img = new Image();
   img.crossOrigin = 'Anonymous';
   img.src = bgImage.value;
+
+  countdown.value =3;
   
   img.onload = async () => {
     // DOM 업데이트가 완료된 후에 실행
     await nextTick();
   
-    html2canvas(captureArea.value, { useCORS: true, allowTaint: false })
-      .then((canvas) => {
-        const imageData = canvas.toDataURL('image/png');
-        images.value.push(imageData);
-
-        if (images.value.length === 10) {
-          alert('이미지 리스트의 길이가 10이 되었습니다.');
-        }
-      })
-      .catch((error) => {
-        console.error('이미지 캡쳐 에러 발생: ', error);
-      });
+    Swal.fire({
+      title: `<h1 style='color:white;'>포즈!</h1>`,
+      html: `<p style='color:white; font-size:50px;'>${countdown.value}</p>`, //text 부분
+      showConfirmButton: false, // 기본 버튼을 없애는 것
+      background: 'rgba(0, 0, 0, 0.3)',  // 모달 배경을 투명하게 설정
+      backdrop: false,  // 모달 배경 투명도 조정
+      didOpen: () => {
+        startCountdown();
+      }
+    });
   };
 
-  img.onerror = (error) => {
+  img.onerror = async (error) => {
     console.error('배경 로딩 에러 발생: ', error);
+    await Swal.fire({
+      title: '@배경 오류 발생@',         // Alert 제목
+      text: '해당 사진은 배경으로 사용할 수 없습니다!',  
+      icon: 'warning',
+    });
   };
 };
 
-//촬영 종료
-const exitphoto = async () =>{
+// 사진 캡처 함수
+const capturePhoto = async () => {
+  console.log('사진 캡처 시작');
+  await nextTick();
+  cameraaudio.play();
+  html2canvas(captureArea.value, { useCORS: true, allowTaint: false })
+    .then( async (canvas) => {
+      const imageData = canvas.toDataURL('image/png');
+      images.value.push(imageData);
+      remainPicCnt.value = 10-images.value.length;
+      if (images.value.length === 10) {
+        const {value: result} = await Swal.fire({
+          title: '사진 촬영 종료',         // Alert 제목
+          text: '10장을 모두 촬영하여 프레임 선택창으로 이동합니다!',  
+          icon: 'success',
+        });
+        if(result){
+          router.push('/')
+        }
+      }
+    })
+    .catch((error) => {
+      console.error('이미지 캡쳐 에러 발생: ', error);
+    });
+};
+
+// 촬영 종료
+const exitphoto = async () => {
   console.log('촬영종료');
 
   const { value: result } = await Swal.fire({
         title: '촬영 끝내기',
-        text: '촬영을 동료하고 저장을 위해 나가시겠습니까?',
+        text: '촬영을 종료하고 저장을 위해 나가시겠습니까?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonText: '확인',
@@ -152,7 +203,7 @@ const exitphoto = async () =>{
       if (result) {
         // 확인 버튼 클릭 시 실행되는 코드
         Swal.fire('저장', '사진 선택 화면으로 이동합니다!', 'success');
-        router.push('/')
+        router.push('/') // 사진 선택 화면으로 이동하도록 변경 필요
       } else {
         // 취소 버튼 클릭 시 실행되는 코드
         Swal.fire('취소', '촬영을 계속합니다!', 'error');
@@ -162,23 +213,31 @@ const exitphoto = async () =>{
 // 컴포넌트 변경을 위한 변수 1- 배경선택, 2 - 사진 보기
 const showtype = ref(1);
 
-const changeComponent = () =>{
+const changeComponent = () => {
   showtype.value = showtype.value === 1 ? 2 : 1; // showtype 토글
   navigateTo(showtype.value === 1 ? 'background' : 'showphoto');
 }
-
 </script>
 
 <template>
   <WhiteBoardComp class="whiteboard-area-booth">
     <div class="booth-content">
-      <div class="close-btn">
-        <button class="close" @click="navigateTo('main')">X</button> 
+      <div class="booth-top-div">
+        <div>
+          남은 사진 수: {{ remainPicCnt }}/10
+        </div>
+        <div class="close-btn">
+          <button class="close" @click="navigateTo('main')">나가기</button> 
+        </div>
       </div>
 
       <div class="booth-content-main">
         <BoothBack class="booth-camera-box">
-          <div ref="captureArea" :style="{ backgroundImage: `url(${bgImage})` }" class="photo-zone" ></div>
+          <div ref="captureArea" :style="{ backgroundImage: `url(${bgImage})` }" class="photo-zone" >
+            <div v-show="isvideoOn">
+                  <video ref="videoElement" autoplay  style="width: 30%; height: fit-content;"></video>
+                </div> 
+          </div>
 
           <div class="create-btn">
             <div class="left-btn">
@@ -262,6 +321,24 @@ const changeComponent = () =>{
 
     width: 100%;
     height: 100%;
+}
+.booth-top-div{
+  width: 95%;
+  height: 8%;
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+  justify-content: space-between;
+
+  .close-btn{
+    padding: 5px;
+    display: flex;
+    justify-content: right;
+    .close{
+      background-color: transparent;
+      border:none;
+    }
+  }
 }
 
 .booth-content-main{
@@ -359,12 +436,9 @@ padding: 5px;
   }
 
 }
-.close-btn{
-width: 90%;
-display: flex;
-justify-content: right;
-  .close{
-    border:none;
-  }
+.timer-modal{
+  box-shadow: none;
+  border: none;
+  color: rgb(255, 255, 255);
 }
 </style>
