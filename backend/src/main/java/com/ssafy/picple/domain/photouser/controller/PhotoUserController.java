1ad3fb2c52d3c2accb9b4117e4b37d27c092b121
염규영 <yeomky98@naver.com>
2024-07-25 14:45:05 +0900

    [#21] feat: add photo user controller

--- backend/src/main/java/com/ssafy/picple/domain/photouser/controller/PhotoUserController.java
+++ backend/src/main/java/com/ssafy/picple/domain/photouser/controller/PhotoUserController.java
@@ -1,7 +1,29 @@
 package com.ssafy.picple.domain.photouser.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.picple.config.baseResponse.BaseException;
+import com.ssafy.picple.config.baseResponse.BaseResponse;
+import com.ssafy.picple.domain.photouser.dto.request.PhotoUserRequestDto;
+import com.ssafy.picple.domain.photouser.dto.response.PhotoUserResponseDto;
+import com.ssafy.picple.domain.photouser.service.PhotoUserService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/photo_user")
 public class PhotoUserController {
+
+	private final PhotoUserService photoUserService;
+
+	@PostMapping
+	public BaseResponse<PhotoUserResponseDto> createPhotoUser(@RequestBody PhotoUserRequestDto requestDto) throws
+			BaseException {
+		PhotoUserResponseDto responseDto = photoUserService.createPhotoUser(requestDto);
+		return new BaseResponse<>(responseDto);
+	}
 }
