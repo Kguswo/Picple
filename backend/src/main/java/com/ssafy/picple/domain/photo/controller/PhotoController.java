7c360f13e3dcaa6e18f1112695089be833fcc694
Now <khj990322@gmail.com>
2024-07-29 02:15:44 +0900

    [#22] feat: Update PhotoController

--- backend/src/main/java/com/ssafy/picple/domain/photo/controller/PhotoController.java
+++ backend/src/main/java/com/ssafy/picple/domain/photo/controller/PhotoController.java
@@ -1,10 +1,13 @@
 package com.ssafy.picple.domain.photo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.picple.domain.photouser.entity.PhotoUser;
+import com.ssafy.picple.domain.photouser.repository.PhotoUserRepository;
+import com.ssafy.picple.domain.user.entity.User;
+import com.ssafy.picple.domain.user.repository.UserRepository;
+import com.ssafy.picple.domain.user.service.UserService;
+import com.ssafy.picple.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.ssafy.picple.AwsS3.S3FileUploadService;
@@ -24,12 +27,15 @@ public class PhotoController {
 
 	private final PhotoService photoService;
 	private final S3FileUploadService s3FileUploadService;
+	private final JWTUtil jwtUtil;
+	private final PhotoUserRepository photoUserRepository;
+	private final UserRepository userRepository;
 
 	// 사진 저장
-	// 사람마다 각자 저장하는데 사진URL자체는 같기 때문에 이거 고민하고 수정해야함
+	// 사람마다 각자 저장하는데 사진URL자체는 같기 때문에 이거 고민하고 수정해야함 -> 시간만 갱신되고 추가되지 않는것 확인
 	@PostMapping(value = "", consumes = "multipart/form-data")
-	public BaseResponse<?> savePhoto(@RequestPart("photo") @Valid Photo photo,
-			@RequestPart("file") MultipartFile file) {
+//	public BaseResponse<?> savePhoto(@RequestPart("photo") @Valid Photo photo, @RequestPart("file") MultipartFile file, @RequestHeader String token) {
+	public BaseResponse<?> savePhoto(@RequestPart("photo") @Valid Photo photo, @RequestPart("file") MultipartFile file) {
 		try {
 			String photoUrl = s3FileUploadService.uploadFile(file);
 
@@ -38,7 +44,26 @@ public class PhotoController {
 					.isShared(false)
 					.isDeleted(false)
 					.build();
-			return new BaseResponse<>(photoService.insertPhoto(newPhoto));
+			Photo savedPhoto = photoService.insertPhoto(newPhoto);
+
+			// PhotoUser 생성 및 저장 - 자동생성 안돼서 만들어줌, userId는 jwt 토큰으로부터
+
+//			Long userId = jwtUtil.getUserId(token);
+//			User user = userRepository.findById(userId)
+//					.orElseThrow(() -> new IllegalArgumentException(BaseResponseStatus.GET_USER_EMPTY.getMessage()));
+
+			// TODO
+			// 일단 USERID 1로 임시 테스트 수정해야함
+			User user = userRepository.findById(1L).get();
+
+			PhotoUser photoUser = PhotoUser.builder()
+					.photo(savedPhoto)
+					.user(user)
+					.content("") // 생성시 빈 값
+					.build();
+			photoUserRepository.save(photoUser);
+
+			return new BaseResponse<>(savedPhoto);
 		} catch (Exception e) {
 			return new BaseResponse<>(BaseResponseStatus.FILE_UPLOAD_ERROR);
 		}
