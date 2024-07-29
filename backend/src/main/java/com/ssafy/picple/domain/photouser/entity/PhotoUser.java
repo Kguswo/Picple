31442abe6e568571293fe9097679afccf5427722
Kguswo <khj990322@gmail.com>
2024-07-29 17:57:43 +0900

    [#22] refactor: Refactor PhotoUser

--- backend/src/main/java/com/ssafy/picple/domain/photouser/entity/PhotoUser.java
+++ backend/src/main/java/com/ssafy/picple/domain/photouser/entity/PhotoUser.java
@@ -3,12 +3,22 @@ package com.ssafy.picple.domain.photouser.entity;
 import com.ssafy.picple.domain.photo.entity.Photo;
 import com.ssafy.picple.domain.user.entity.User;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "photo_user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PhotoUser {
@@ -36,4 +46,7 @@ public class PhotoUser {
 		this.content = content;
 	}
 
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
