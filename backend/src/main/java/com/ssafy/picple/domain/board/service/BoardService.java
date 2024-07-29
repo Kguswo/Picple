dbcb407b358cd5733490b089a5707fff2956ba10
Kguswo <khj990322@gmail.com>
2024-07-29 17:54:58 +0900

    [#22] refactor: Refactor Board

--- backend/src/main/java/com/ssafy/picple/domain/board/service/BoardService.java
+++ backend/src/main/java/com/ssafy/picple/domain/board/service/BoardService.java
@@ -2,20 +2,26 @@ package com.ssafy.picple.domain.board.service;
 
 import java.util.List;
 
+import com.ssafy.picple.config.baseResponse.BaseException;
 import com.ssafy.picple.domain.board.dto.BoardDto;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface BoardService {
 
 	// Board 전체 조회
-	List<BoardDto> findAllBoards();
+	List<BoardDto> findAllBoards(Long userId);
 
 	// 사용자 기준에 따라 정렬
-	List<BoardDto> findAllBoardsOrderByMyCriteria(String criteria);
+	List<BoardDto> findAllBoardsOrderByMyCriteria(Long userId, String criteria);
 
 	// 사용자 닉네임 검색으로 해당 유저(닉네임) 포함된 사진 조회
-	List<BoardDto> findAllBoardsByUserNickname(String nickname);
+	List<BoardDto> findAllBoardsByUserNickname(Long userId, String nickname);
 
 	// 내가 올린 사진 삭제하기
 	boolean deleteBoard(Long boardId, Long userId);
+
+	// 접속한 유저 아이디
+	Long getUserId(HttpServletRequest request) throws BaseException;
 
 }
