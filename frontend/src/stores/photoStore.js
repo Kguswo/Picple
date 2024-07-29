ff8ef9c3cace6f160ce24d49965dde7ae05d0398
김예훈 <rladpgns00@gmail.com>
2024-07-29 11:09:14 +0900

    [#30] refactor: pinia 코드는 setup 방식으로 통일

--- frontend/src/stores/photoStore.js
+++ frontend/src/stores/photoStore.js
@@ -1,15 +1,19 @@
 import { defineStore } from "pinia";
 
-export const usePhotoStore = defineStore("photo", {
-    state: () => ({
-        photoList: [],
-    }),
-    actions: {
-        setPhotoList(photos) {
-            this.photoList = photos;
-        },
-        clearPhotoList() {
-            this.photoList = [];
-        },
-    },
+export const usePhotoStore = defineStore("photo", () => {
+	const photoList = [];
+
+	const setPhotoList = (photos) => {
+		photoList = photos;
+	};
+
+	const clearPhotoList = () => {
+		photoList = [];
+	};
+
+	return {
+		photoList,
+		setPhotoList,
+		clearPhotoList,
+	};
 });
