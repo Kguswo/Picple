8a6752d5da0533bb450373252af824360fe563d7
Kguswo <khj990322@gmail.com>
2024-07-29 17:56:21 +0900

    [#22] refactor: Refactor BoardLike

--- backend/src/main/java/com/ssafy/picple/domain/boardlike/controller/BoardLikeController.java
+++ backend/src/main/java/com/ssafy/picple/domain/boardlike/controller/BoardLikeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.picple.config.baseResponse.BaseException;
 import com.ssafy.picple.config.baseResponse.BaseResponse;
 import com.ssafy.picple.config.baseResponse.BaseResponseStatus;
 import com.ssafy.picple.domain.boardlike.service.BoardLikeService;
@@ -15,19 +16,35 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/like")
+@RequestMapping("/likes")
 public class BoardLikeController {
 
 	private final BoardLikeService likeService;
 
+	/**
+	 * 좋아요 및 숫자 1 증가
+	 *
+	 * @param boardId
+	 * @param userId
+	 * @return
+	 */
 	@GetMapping("/{boardId}")
-	public BaseResponse<Boolean> isPhotoLikedByUser(@PathVariable Long boardId, @RequestParam Long userId) {
+	public BaseResponse<Boolean> isPhotoLikedByUser(@PathVariable Long boardId,
+			@RequestParam Long userId) throws BaseException {
 		boolean isLiked = likeService.isPhotoLikedByUser(boardId, userId);
 		return new BaseResponse<>(isLiked);
 	}
 
+	/**
+	 * 좋아요 취소 및 숫자 1 감소
+	 *
+	 * @param boardId
+	 * @param userId
+	 * @return
+	 */
 	@PatchMapping("/{boardId}")
-	public BaseResponse<?> changeIsLiked(@PathVariable Long boardId, @RequestParam(required = false) Long userId) {
+	public BaseResponse<?> changeIsLiked(@PathVariable Long boardId,
+			@RequestParam(required = false) Long userId) throws BaseException {
 		if (userId == null) {
 			return new BaseResponse<>(BaseResponseStatus.GET_USER_EMPTY);
 		}
