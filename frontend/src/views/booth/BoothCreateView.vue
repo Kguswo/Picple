5dd079c76bd420f207d7f1638f4727f37ce999f9
김예훈 <rladpgns00@gmail.com>
2024-07-29 15:19:44 +0900

    refactor: navigateTo 함수의 인자 이름이 path 였던 것을 name으로 수정

--- frontend/src/views/booth/BoothCreateView.vue
+++ frontend/src/views/booth/BoothCreateView.vue
@@ -7,28 +7,28 @@ import videoOff from '@/assets/icon/video_off.png';
 import microOn from '@/assets/icon/micro_on.png';
 import microOff from '@/assets/icon/micro_off.png';
 
-import { RouterView, RouterLink, useRoute,useRouter } from "vue-router";
+import { RouterView, RouterLink, useRoute, useRouter } from "vue-router";
 import { ref, onMounted, onUnmounted } from 'vue';
 
 // 비디오 표현을 위한 변수
-const videoElement = ref(null); 
+const videoElement = ref(null);
 let mediaStream = null;
 
 // 화면 표시에 있어 사용되는 변수
 let isMirrored = false;
-let isvideoOn = ref(true); 
+let isvideoOn = ref(true);
 let isMicroOn = ref(true);
 
 const router = useRouter();
 
-const navigateTo = (path) => {
-    router.push({ name: path });
+const navigateTo = (name) => {
+  router.push({ name });
 };
 
 onMounted(async () => {
   console.log("Create Booth 페이지 호출되었습니다");
   try {
-    mediaStream = await navigator.mediaDevices.getUserMedia({ video: true,audio: true });
+    mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     videoElement.value.srcObject = mediaStream;
   } catch (error) {
     console.error('Error accessing webcam:', error);
@@ -65,7 +65,7 @@ const toggleCamera = () => {
     mediaStream.getVideoTracks().forEach((track) => {
       track.enabled = false;  // 비디오 트랙 비활성화
     });
-    videoElement.value.srcObject = mediaStream; 
+    videoElement.value.srcObject = mediaStream;
   }
 };
 
@@ -93,122 +93,126 @@ const toggleMicro = () => {
   <WhiteBoardComp class="whiteboard-area-booth">
     <div class="booth-content">
       <div class="close-btn">
-        <button class="close" @click="navigateTo('main')">closeBtn</button> 
-    </div>
-    <BoothBack class="booth-create" >
+        <button class="close" @click="navigateTo('main')">closeBtn</button>
+      </div>
+      <BoothBack class="booth-create">
         <div class="create-content">
-            <div class="mycam-box">
-              <!-- v-if로 하면 카메라가 나오지 않아 v-show로 미리 렌더링 -->
-                <div v-show="isvideoOn">
-                  <video ref="videoElement" autoplay></video>
-                </div> 
-                <div v-if="!isvideoOn">카메라가 꺼져있습니다!</div>
+          <div class="mycam-box">
+            <!-- v-if로 하면 카메라가 나오지 않아 v-show로 미리 렌더링 -->
+            <div v-show="isvideoOn">
+              <video ref="videoElement" autoplay></video>
             </div>
+            <div v-if="!isvideoOn">카메라가 꺼져있습니다!</div>
+          </div>
 
 
-            <div class="create-btn">
-                <div class="left-btn">
-                    <button class="circle-btn" @click="toggleMicro">
-                      <img :src="isMicroOn ? microOn : microOff"  alt="M">
-                    </button>
-                    <button class="circle-btn" @click="toggleCamera">
-                      <img :src="isvideoOn ? videoOn : videoOff"  alt="C">
-                    </button>
+          <div class="create-btn">
+            <div class="left-btn">
+              <button class="circle-btn" @click="toggleMicro">
+                <img :src="isMicroOn ? microOn : microOff" alt="M">
+              </button>
+              <button class="circle-btn" @click="toggleCamera">
+                <img :src="isvideoOn ? videoOn : videoOff" alt="C">
+              </button>
 
-                    <button class="ract-btn" @click="toggleMirror">반전</button>
-                </div>
-                <div class="right-btn">
-                    <button class="ract-btn" @click="navigateTo('background')">생성</button>
-                    <button class="ract-btn" @click="navigateTo('main')">취소</button>
-                </div>
+              <button class="ract-btn" @click="toggleMirror">반전</button>
             </div>
+            <div class="right-btn">
+              <button class="ract-btn" @click="navigateTo('background')">생성</button>
+              <button class="ract-btn" @click="navigateTo('main')">취소</button>
+            </div>
+          </div>
         </div>
-    </BoothBack>
-    
+      </BoothBack>
+
     </div>
   </WhiteBoardComp>
 </template>
 
 <style scoped>
-.booth-content{
+.booth-content {
 
-    /* display */
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
+  /* display */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
 
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 }
 
-.create-content{
+.create-content {
 
-display: flex;
-flex-direction: column;
-justify-content: space-evenly;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
 
-height: 100%;
-width: 100%;
-}
-.mycam-box{
-margin-top: 15px ;
-height: 80%;
-width: 90%;
-min-height: 350px;
-min-width: 700px;
-background-color: rgb(255, 255, 255);
-
-border-radius: 20px;
-
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-
-}
-.create-btn{
-height: 10%;
-width: 90%;
-display: flex;
-justify-content: space-between;
+  height: 100%;
+  width: 100%;
 }
 
-.circle-btn{
-border:none;
-border-radius: 50%;
-width: 50px;
-height: 50px;
-line-height: 50px;
-padding: 5px;
-display: flex; 
-justify-content: center; 
-align-items: center; 
-margin: 0 5px;
-border: none; 
-background-color: transparent;
+.mycam-box {
+  margin-top: 15px;
+  height: 80%;
+  width: 90%;
+  min-height: 350px;
+  min-width: 700px;
+  background-color: rgb(255, 255, 255);
+
+  border-radius: 20px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
 }
 
-.ract-btn{
-border:none;
-border-radius: 20px;
-width: 75px;
-height: 50px;
-margin: 5px;
-padding: 5px;
+.create-btn {
+  height: 10%;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+}
 
-&:hover{
+.circle-btn {
+  border: none;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  padding: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  border: none;
+  background-color: transparent;
+}
+
+.ract-btn {
+  border: none;
+  border-radius: 20px;
+  width: 75px;
+  height: 50px;
+  margin: 5px;
+  padding: 5px;
+
+  &:hover {
     background-color: rgb(136, 136, 136)
-}
+  }
 
 }
-.left-btn{
+
+.left-btn {
   display: flex;
 }
-.close-btn{
-width: 90%;
-display: flex;
-justify-content: right;
+
+.close-btn {
+  width: 90%;
+  display: flex;
+  justify-content: right;
 }
 </style>
