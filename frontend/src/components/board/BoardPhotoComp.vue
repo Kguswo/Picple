bbbfd5547a822a2be7b88a9add0fab65f798c0a5
김예훈 <rladpgns00@gmail.com>
2024-07-30 13:25:51 +0900

    [#32] feat: 게시판 정렬 api 연결

--- frontend/src/components/board/BoardPhotoComp.vue
+++ frontend/src/components/board/BoardPhotoComp.vue
@@ -1,40 +1,50 @@
 <script setup>
 import { ref } from 'vue';
+
 const props = defineProps({
-    photos: Array,
+    photo: Object,
 })
 
 const showModal = ref(false);
 const currentItem = ref(null);
 
-const picZoom = (item) => {
+const picZoom = () => {
     showModal.value = true;
-    currentItem.value = item;
+    currentItem.value = props.photo;
 };
 
 const closeModal = () => {
     showModal.value = false;
     currentItem.value = null;
 };
+
+const toggleLike = () => {
+    if (props.photo.liked) {
+        --props.photo.hit;
+    } else {
+        ++props.photo.hit;
+    }
+    props.photo.liked = !props.photo.liked
+}
 </script>
 
 <template>
-    <div class="photo-card" v-for="item in photos" :key="item.id" @click="picZoom(item)">
-        <div class="photo">
+    <div class="photo-card">
+        <div class="photo" @click="picZoom">
         </div>
         <div class="content">
             <div class="like">
-                <svg v-if="item.liked" xmlns="@/assets/icon/hear-fill.svg" class="heart" width="20" height="20"
-                    fill="currentColor" viewBox="0 0 16 16">
+                <svg v-if="photo.liked" xmlns="@/assets/icon/hear-fill.svg" class="heart" width="20" height="20"
+                    fill="red" viewBox="0 0 16 16" @click="toggleLike">
                     <path fill-rule="evenodd"
                         d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
                 </svg>
-                <svg v-else xmlns="@/assets/icon/hear.svg" width="20" height="20" fill="currentColor" class="heart"
-                    viewBox="0 0 16 16">
+                <svg v-else xmlns="@/assets/icon/hear.svg" width="20" height="20" fill="red" class="heart"
+                    viewBox="0 0 16 16" @click="toggleLike">
                     <path
                         d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
                 </svg>
-                <span class="like-cnt">{{ item.hit }}</span>
+                <span class="like-cnt">{{ photo.hit }}</span>
             </div>
 
         </div>
@@ -48,8 +58,7 @@ const closeModal = () => {
             <div class="modal-img">
                 <img src="@/assets/img/tempImg.png" alt="">
                 <div class="modal-text">
-                    <span class="modal-like">Like. 22</span>
-                    <span class="modal-date">Date. 2024.07.19</span>
+                    <span class="modal-date">촬영일: {{ currentItem.createdAt }}</span>
                 </div>
             </div>
         </div>
