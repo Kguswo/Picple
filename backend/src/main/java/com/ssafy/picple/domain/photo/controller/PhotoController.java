293a3fe6fc57220245a4975e450881bbe73aac93
Kguswo <khj990322@gmail.com>
2024-07-30 14:14:08 +0900

    [#22] refactor: Refactor PhotoController

--- backend/src/main/java/com/ssafy/picple/domain/photo/controller/PhotoController.java
+++ backend/src/main/java/com/ssafy/picple/domain/photo/controller/PhotoController.java
@@ -1,29 +1,20 @@
 package com.ssafy.picple.domain.photo.controller;
 
-import com.ssafy.picple.config.baseResponse.BaseException;
-import com.ssafy.picple.domain.board.service.BoardService;
-import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.ssafy.picple.AwsS3.S3FileUploadService;
+import com.ssafy.picple.config.baseResponse.BaseException;
 import com.ssafy.picple.config.baseResponse.BaseResponse;
-import com.ssafy.picple.config.baseResponse.BaseResponseStatus;
 import com.ssafy.picple.domain.photo.entity.Photo;
 import com.ssafy.picple.domain.photo.service.PhotoService;
-import com.ssafy.picple.domain.photouser.entity.PhotoUser;
-import com.ssafy.picple.domain.photouser.repository.PhotoUserRepository;
-import com.ssafy.picple.domain.user.entity.User;
-import com.ssafy.picple.domain.user.repository.UserRepository;
-import com.ssafy.picple.util.JWTUtil;
 
-import jakarta.validation.Valid;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/photos")
@@ -31,11 +22,11 @@ import java.io.IOException;
 public class PhotoController {
 
 	private final PhotoService photoService;
-	private final BoardService boardService;
 
 	/**
 	 * 사진 저장
-	 * 사람마다 각자 저장하는데 사진URL자체는 같기 때문에 이거 고민하고 수정해야함 -> 시간만 갱신되고 추가되지 않는것 확인
+	 * 사람마다 각자 저장하는데 사진URL자체는 같기 때문에 수정 -> 시간만 갱신되고 추가되지 않는것 확인
+	 *
 	 * @param request
 	 * @param file
 	 * @return
@@ -44,10 +35,9 @@ public class PhotoController {
 	 */
 	@PostMapping(value = "", consumes = "multipart/form-data")
 	public BaseResponse<Photo> savePhoto(HttpServletRequest request,
-									 @RequestPart("file") MultipartFile file) throws BaseException, IOException {
+			@RequestPart("file") MultipartFile file) throws BaseException, IOException {
 
-//		Long userId = boardService.getUserId(request);
-		Long userId = (Long) request.getAttribute("userId");
+		Long userId = (Long)request.getAttribute("userId");
 		System.out.println("userId = " + userId);
 		Photo savedPhoto = photoService.insertPhoto(userId, file);
 
