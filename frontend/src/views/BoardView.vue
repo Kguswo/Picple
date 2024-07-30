0373291bc5d05a300cf499f545f9a38713e4dd61
김예훈 <rladpgns00@gmail.com>
2024-07-30 13:45:33 +0900

    [#32] feat: 게시판 닉네임 검색 api 연결

--- frontend/src/views/BoardView.vue
+++ frontend/src/views/BoardView.vue
@@ -3,7 +3,8 @@ import WhiteBoardComp from "@/components/common/WhiteBoardComp.vue";
 import BoardPhotoComp from "@/components/board/BoardPhotoComp.vue";
 import Page from "@/components/common/PageComp.vue";
 import { onMounted, ref } from "vue";
-import { boardSortApi } from "@/api/boardApi";
+import { boardSearchApi, boardSortApi } from "@/api/boardApi";
+import Swal from "sweetalert2";
 
 const photos = ref([]);
 const likeClicked = ref(false);
@@ -14,19 +15,26 @@ onMounted(async () => {
     photos.value = data.result;
 })
 
-const nickname = ref({ type: "text", label: "", value: "" });
+const nickname = ref("");
 
-const submitForm = () => {
-    if (nickname.value == "") {
-        window.alert("값을 입력해주세요!");
+const searchByNickname = async () => {
+    if (!nickname.value) {
+        return;
     }
-    else {
-        console.log('전송됨', nickname.value)
+    const data = await boardSearchApi(nickname.value);
+    if (!data.isSuccess) {
+        await Swal.fire({ icon: "error", title: "검색 조회에 실패하였습니다.", width: 600 });
+        return;
     }
+    photos.value = data.result;
 }
 
 const sortByCreatedAt = async () => {
     const data = await boardSortApi("createdAt")
+    if (!data.isSuccess) {
+        await Swal.fire({ icon: "error", title: "최신순 정렬에 실패하였습니다.", width: 600 });
+        return;
+    }
     photos.value = data.result;
     timeClicked.value = true;
     likeClicked.value = false;
@@ -34,6 +42,10 @@ const sortByCreatedAt = async () => {
 
 const sortByHit = async () => {
     const data = await boardSortApi("hit")
+    if (!data.isSuccess) {
+        await Swal.fire({ icon: "error", title: "좋아요순 정렬에 실패하였습니다.", width: 600 });
+        return;
+    }
     photos.value = data.result;
     likeClicked.value = true;
     timeClicked.value = false;
@@ -48,22 +60,23 @@ const sortByHit = async () => {
 
             <div class="board-area">
                 <div class="button-box">
-                    <form @submit.prevent="submitForm">
+                    <form @submit.prevent="searchByNickname">
                         <div class="input-container">
-                            <input type="text" name="nickname" placeholder=" 닉네임을 입력해주세요!" class="form-input"
-                                maxlength="8" autocomplete="off" />
-                            <button type="button" class="form-button-small">검색</button>
+                            <input type="text" name="nickname" v-model="nickname" placeholder=" 닉네임을 입력해주세요!"
+                                class="form-input" maxlength="8" autocomplete="off" />
+                            <button type="button" class="form-button-small" @click="searchByNickname">검색</button>
                         </div>
                     </form>
 
                     <div class="btn-group">
-                        <button class="likeSort" :class="{ clicked: likeClicked }" @click="sortByHit">좋아요 순</button>
+                        <button class="likeSort" :class="{ clicked: likeClicked }" @click="sortByHit">좋아요순</button>
                         <button class="timeSort" :class="{ clicked: timeClicked }" @click="sortByCreatedAt">최신순</button>
                     </div>
                 </div>
 
                 <div class="board">
-                    <BoardPhotoComp v-for="photo in photos" :key="photo.id" :photo="photo" />
+                    <div v-if="photos.length === 0" style="font-size: 50px">게시물 없음</div>
+                    <BoardPhotoComp v-else v-for="photo in photos" :key="photo.id" :photo="photo" />
                 </div>
             </div>
         </WhiteBoardComp>
