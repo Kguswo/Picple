7a263c081fc09ece844492ea4731d3c10f142ec5
김예훈 <rladpgns00@gmail.com>
2024-07-30 17:52:20 +0900

    [#32] feat: 로그아웃 기능 추가

--- frontend/src/components/common/MainHeaderComp.vue
+++ frontend/src/components/common/MainHeaderComp.vue
@@ -1,16 +1,22 @@
 <script setup>
 import { useRouter } from "vue-router";
 import { useUserStore } from "@/stores/userStore";
+import { storeToRefs } from "pinia";
 
 const router = useRouter();
 const userStore = useUserStore();
+
+const { userInfo } = storeToRefs(userStore);
 
 const navigateTo = (name) => {
     router.push({ name });
 };
 
 const logout = () => {
-    // todo: 로그아웃 구현
+    localStorage.removeItem("accessToken");
+    userInfo.value.email = "";
+    userInfo.value.nickname = "";
+    window.location.href = "/";
 }
 </script>
 
@@ -21,8 +27,8 @@ const logout = () => {
                 <img src="@/assets/img/mainView/picpleLogo.png" alt="" @click="navigateTo('main')" />
             </div>
             <div class="right">
-                <div v-if="userStore.userInfo.nickname" class="dropdown">
-                    <span>{{ userStore.userInfo.nickname }}</span>
+                <div v-if="userInfo.nickname" class="dropdown">
+                    <span>{{ userInfo.nickname }}</span>
                     <div class="dropdown-content">
                         <button type="button" @click="navigateTo('modifyAccount')" class="navbar-button">정보 수정</button>
                         <br />
