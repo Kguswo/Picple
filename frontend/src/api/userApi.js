5ebacadb9118ee7f43b5d13e7cb94134cae0d42d
김예훈 <rladpgns00@gmail.com>
2024-07-30 10:43:27 +0900

    [#32] feat: 회원가입 api 프론트엔드 연결

---
+++ frontend/src/api/userApi.js
@@ -0,0 +1,31 @@
+import { instance } from "@/api/baseApi";
+import axios from "axios";
+
+const userBaseUrl = axios.defaults.baseURL + "/user";
+
+const loginApi = async (email, password) => {
+	try {
+		const response = await instance.post(`${userBaseUrl}/login`, {
+			email,
+			password,
+		});
+		return response.data;
+	} catch (error) {
+		return error;
+	}
+};
+
+const signupApi = async (email, password, nickname) => {
+	try {
+		const response = await instance.post(`${userBaseUrl}/sign-up`, {
+			email,
+			password,
+			nickname,
+		});
+		return response.data;
+	} catch (error) {
+		return error;
+	}
+};
+
+export { loginApi, signupApi };
