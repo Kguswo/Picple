21661f3df34e7e3e3a950409ef8ca3fe73e978c7
Kguswo <khj990322@gmail.com>
2024-07-30 14:12:35 +0900

    [#22] feat: Update BoardLikeController

--- backend/src/main/java/com/ssafy/picple/domain/boardlike/controller/BoardLikeController.java
+++ backend/src/main/java/com/ssafy/picple/domain/boardlike/controller/BoardLikeController.java
@@ -4,14 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.picple.config.baseResponse.BaseException;
 import com.ssafy.picple.config.baseResponse.BaseResponse;
 import com.ssafy.picple.config.baseResponse.BaseResponseStatus;
+import com.ssafy.picple.domain.board.service.BoardService;
 import com.ssafy.picple.domain.boardlike.service.BoardLikeService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,45 +21,53 @@ import lombok.RequiredArgsConstructor;
 public class BoardLikeController {
 
 	private final BoardLikeService likeService;
+	private final BoardService boardService;
+
 
 	/**
-	 * 좋아요 및 숫자 1 증가
+	 * 좋아요 여부 체크
 	 *
+	 * @param request
 	 * @param boardId
-	 * @param userId
 	 * @return
+	 * @throws BaseException
 	 */
 	@GetMapping("/{boardId}")
-	public BaseResponse<Boolean> isPhotoLikedByUser(@PathVariable Long boardId,
-			@RequestParam Long userId) throws BaseException {
+	public BaseResponse<Boolean> isPhotoLikedByUser(HttpServletRequest request,
+			@PathVariable Long boardId) throws BaseException {
+		Long userId = (Long)request.getAttribute("userId");
 		boolean isLiked = likeService.isPhotoLikedByUser(boardId, userId);
 		return new BaseResponse<>(isLiked);
 	}
 
 	/**
-	 * 좋아요 취소 및 숫자 1 감소
+	 * 좋아요 시 : 좋아요 및 숫자 1 증가
+	 * 좋아요 취소시 : 좋아요 취소 및 숫자 1 감소
 	 *
+	 * @param request
 	 * @param boardId
-	 * @param userId
 	 * @return
+	 * @throws BaseException
 	 */
 	@PatchMapping("/{boardId}")
-	public BaseResponse<?> changeIsLiked(@PathVariable Long boardId,
-			@RequestParam(required = false) Long userId) throws BaseException {
-		if (userId == null) {
-			return new BaseResponse<>(BaseResponseStatus.GET_USER_EMPTY);
-		}
-		try {
-			boolean isLiked = likeService.isPhotoLikedByUser(boardId, userId);
-			if (isLiked) {
-				likeService.unlikePhoto(boardId, userId);
-			} else {
-				likeService.likePhoto(boardId, userId);
+	public BaseResponse<?> changeIsLiked(HttpServletRequest request,
+			@PathVariable Long boardId) throws BaseException {
+		Long userId = (Long)request.getAttribute("userId");
+		if (userId != null) {
+			try {
+				boolean isLiked = likeService.isPhotoLikedByUser(boardId, userId);
+				if (isLiked) {
+					likeService.unlikePhoto(boardId, userId);
+				} else {
+					likeService.likePhoto(boardId, userId);
+				}
+				return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+			} catch (Exception e) {
+				e.printStackTrace();
+				return new BaseResponse<>(BaseResponseStatus.RESPONSE_ERROR);
 			}
-			return new BaseResponse<>(BaseResponseStatus.SUCCESS);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new BaseResponse<>(BaseResponseStatus.RESPONSE_ERROR);
+		} else {
+			return new BaseResponse<>(BaseResponseStatus.GET_USER_EMPTY);
 		}
 	}
 
