0292247adc605444bce9e610cbce13a8c6d990d8
김예훈 <rladpgns00@gmail.com>
2024-07-30 17:04:24 +0900

    [#32] feat: pinia 상태 저장을 위한 pinia-persistedstate 플러그인 설치

--- frontend/src/stores/userStore.js
+++ frontend/src/stores/userStore.js
@@ -1,16 +1,22 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useUserStore = defineStore("userStore", () => {
-	const userInfo = ref({
-		email: null,
-		nickname: null,
-	}); // 로그인한 유저의 정보
+export const useUserStore = defineStore(
+	"userStore",
+	() => {
+		const userInfo = ref({
+			email: null,
+			nickname: null,
+		}); // 로그인한 유저의 정보
 
-	const userEmail = ref(null); // 이메일 인증 페이지에서 인증한 이메일
+		const userEmail = ref(null); // 이메일 인증 페이지에서 인증한 이메일
 
-	return {
-		userInfo,
-		userEmail,
-	};
-});
+		return {
+			userInfo,
+			userEmail,
+		};
+	},
+	{
+		persist: true,
+	}
+);
