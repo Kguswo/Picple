cdc1987bd0623b41b0f1e910700c7ccfc6c490dd
김예훈 <rladpgns00@gmail.com>
2024-07-31 16:09:54 +0900

    [#32] fix: 로그인 아이디 비밀번호 불일치 시 에러 메시지 출력 방식 변경

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import { RouterView, RouterLink } from "vue-router";
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-    <div class="main">
-        <RouterView />
-        <!-- <router-link :to="{ name: '' }"></router-link> -->
-    </div>
+	<div class="main">
+		<RouterView />
+	</div>
 </template>
 
 <style scoped>
 .main {
-    position: relative;
-    width: 100vw;
-    height: 100vh;
-    min-width: 1000px;
-    min-height : 700px;
-    
-    background-image: url("@/assets/img/app/backgroundImg.png");
-    background-repeat: no-repeat;
-    background-size: cover;
-    opacity: 1;
+	position: relative;
+	width: 100vw;
+	height: 100vh;
+	min-width: 1000px;
+	min-height: 700px;
 
-    /* display */
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	background-image: url('@/assets/img/app/backgroundImg.png');
+	background-repeat: no-repeat;
+	background-size: cover;
+	opacity: 1;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
