3179301d47f577461495aea08509b324eea45aed
Kguswo <khj990322@gmail.com>
2024-08-01 14:30:47 +0900

    [#36] fix: Fix import Error in JWTInterceptor

--- backend/src/main/java/com/ssafy/picple/interceptor/JWTInterceptor.java
+++ backend/src/main/java/com/ssafy/picple/interceptor/JWTInterceptor.java
@@ -1,39 +1,42 @@
 package com.ssafy.picple.interceptor;
 
+import static com.ssafy.picple.config.baseResponse.BaseResponseStatus.*;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 import com.ssafy.picple.config.baseResponse.BaseException;
 import com.ssafy.picple.util.JWTUtil;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-import static com.ssafy.picple.config.baseResponse.BaseResponseStatus.*;
 
 @Component
 @RequiredArgsConstructor
 public class JWTInterceptor implements HandlerInterceptor {
 
-    private final JWTUtil jwtUtil;
+	private final JWTUtil jwtUtil;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-                             Object handler) throws BaseException {
-        String token = request.getHeader("X-ACCESS-TOKEN");
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object handler) throws BaseException {
+		String token = request.getHeader("X-ACCESS-TOKEN");
 
-        if (CorsUtils.isPreFlightRequest(request)) {
-            return true;
-        }
+		if (CorsUtils.isPreFlightRequest(request)) {
+			return true;
+		}
 
-        if (token == null) {
-            throw new BaseException(EMPTY_JWT);
-        } else if (!jwtUtil.verifyToken(token)) {
-            throw new BaseException(INVALID_JWT);
-        }
+		if (token == null) {
+			throw new BaseException(EMPTY_JWT);
+		} else if (!jwtUtil.verifyToken(token)) {
+			throw new BaseException(INVALID_JWT);
+		}
 
-        Long userId = jwtUtil.getUserId(token);
-        request.setAttribute("userId", userId);
+		Long userId = jwtUtil.getUserId(token);
+		request.setAttribute("userId", userId);
 
-        return true;
-    }
+		return true;
+	}
 }
