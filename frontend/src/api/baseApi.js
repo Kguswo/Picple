45bc5b5e0b5e80500c941f9974dd3f1eedb4ae5a
김예훈 <rladpgns00@gmail.com>
2024-08-06 10:03:40 +0900

    [#40] .env 파일에 api url 추가, gitignore에 .env 추가

--- frontend/src/api/baseApi.js
+++ frontend/src/api/baseApi.js
@@ -3,7 +3,7 @@ import Swal from 'sweetalert2';
 import router from '@/router';
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:8080',
+	baseURL: import.meta.env.VITE_API_URL,
 });
 
 instance.interceptors.request.use(
