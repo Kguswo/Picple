3663d8a37c42730bacec4b703895db5ebf9d7fc5
JinSeok <spdlqjanf@naver.com>
2024-08-07 17:50:22 +0900

    [#47] feat: 오픈비두 테스트

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,136 +2,142 @@ import { createRouter, createWebHistory } from 'vue-router';
 import WebSocketService from '@/services/WebSocketService';
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'main',
-			component: () => import('@/views/MainView.vue'),
-		},
-		{
-			path: '/calendar',
-			name: 'calendarView',
-			component: () => import('@/views/CalendarView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/login',
-			name: 'login',
-			component: () => import('@/views/account/LoginView.vue'),
-			meta: { notAuthRequired: true },
-		},
-		{
-			path: '/signup',
-			name: 'signup',
-			component: () => import('@/views/account/SignupView.vue'),
-			meta: { emailRequired: true, notAuthRequired: true },
-		},
-		{
-			path: '/signup/email',
-			name: 'signupEmail',
-			component: () => import('@/views/account/SignupEmailView.vue'),
-			meta: { notAuthRequired: true },
-		},
-		{
-			path: '/modifyAccount',
-			name: 'modifyAccount',
-			component: () => import('@/views/account/ModifyAccountView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/modifyPassword/:path',
-			name: 'modifyPassword',
-			component: () => import('@/views/account/ModifyPasswordView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/findPassword',
-			name: 'findPassword',
-			component: () => import('@/views/account/FindPasswordView.vue'),
-			meta: { notAuthRequired: true },
-		},
-		{
-			path: '/board',
-			name: 'board',
-			component: () => import('@/views/BoardView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/create',
-			name: 'createbooth',
-			component: () => import('@/views/booth/BoothCreateView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/booth/:boothId',
-			component: () => import('@/views/booth/BoothShootView.vue'),
-			children: [
-				{
-					path: 'bg',
-					name: 'background',
-					component: () => import('@/components/booth/BoothSelectBackComp.vue'),
-					props: true,
-					meta: { authRequired: true },
-				},
-				{
-					path: 'photo',
-					name: 'showphoto',
-					component: () => import('@/components/booth/BoothShowPhotoComp.vue'),
-					props: true,
-					meta: { authRequired: true },
-				},
-			],
-			props: true,
-			meta: { authRequired: true },
-		},
-		{
-			path: '/boothCode',
-			name: 'boothCode',
-			component: () => import('@/views/booth/BoothCodeView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/selectTemp',
-			name: 'selectTemp',
-			component: () => import('@/views/booth/BoothTemplateView.vue'),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/insertImg/:templateKey',
-			name: 'insertImg',
-			component: () => import('@/views/booth/BoothInsertView.vue'),
-			props: (route) => ({
-				templateKey: route.params.templateKey,
-				photos: route.params.photos ? JSON.parse(decodeURIComponent(route.params.photos)) : [],
-			}),
-			meta: { authRequired: true },
-		},
-		{
-			path: '/:pathMatch(.*)*',
-			name: 'notFound',
-			component: () => import('@/views/NotFoundView.vue'),
-		},
-	],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'main',
+            component: () => import('@/views/MainView.vue'),
+        },
+        {
+            path: '/calendar',
+            name: 'calendarView',
+            component: () => import('@/views/CalendarView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: () => import('@/views/account/LoginView.vue'),
+            meta: { notAuthRequired: true },
+        },
+        {
+            path: '/signup',
+            name: 'signup',
+            component: () => import('@/views/account/SignupView.vue'),
+            meta: { emailRequired: true, notAuthRequired: true },
+        },
+        {
+            path: '/signup/email',
+            name: 'signupEmail',
+            component: () => import('@/views/account/SignupEmailView.vue'),
+            meta: { notAuthRequired: true },
+        },
+        {
+            path: '/modifyAccount',
+            name: 'modifyAccount',
+            component: () => import('@/views/account/ModifyAccountView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/modifyPassword/:path',
+            name: 'modifyPassword',
+            component: () => import('@/views/account/ModifyPasswordView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/findPassword',
+            name: 'findPassword',
+            component: () => import('@/views/account/FindPasswordView.vue'),
+            meta: { notAuthRequired: true },
+        },
+        {
+            path: '/board',
+            name: 'board',
+            component: () => import('@/views/BoardView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/create',
+            name: 'createbooth',
+            component: () => import('@/views/booth/BoothCreateView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/booth/:boothId',
+            component: () => import('@/views/booth/BoothShootView.vue'),
+            children: [
+                {
+                    path: 'bg',
+                    name: 'background',
+                    component: () => import('@/components/booth/BoothSelectBackComp.vue'),
+                    props: true,
+                    meta: { authRequired: true },
+                },
+                {
+                    path: 'photo',
+                    name: 'showphoto',
+                    component: () => import('@/components/booth/BoothShowPhotoComp.vue'),
+                    props: true,
+                    meta: { authRequired: true },
+                },
+            ],
+            props: true,
+            meta: { authRequired: true },
+        },
+        {
+            path: '/boothCode',
+            name: 'boothCode',
+            component: () => import('@/views/booth/BoothCodeView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/selectTemp',
+            name: 'selectTemp',
+            component: () => import('@/views/booth/BoothTemplateView.vue'),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/insertImg/:templateKey',
+            name: 'insertImg',
+            component: () => import('@/views/booth/BoothInsertView.vue'),
+            props: (route) => ({
+                templateKey: route.params.templateKey,
+                photos: route.params.photos ? JSON.parse(decodeURIComponent(route.params.photos)) : [],
+            }),
+            meta: { authRequired: true },
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'notFound',
+            component: () => import('@/views/NotFoundView.vue'),
+        },
+        {
+            path: '/videoDisplay/:sessionId',
+            name: 'videoDisplay',
+            component: () => import('@/components/VideoDisplay.vue'),
+            props: true,
+        },
+    ],
 });
 
 // WebSocket 연결이 필요한 라우트 목록
 const websocketRoutes = ['createbooth', 'booth', 'boothCode', 'selectTemp', 'insertImg'];
 
 router.beforeEach(async (to, from, next) => {
-	if (websocketRoutes.includes(to.name)) {
-		if (!WebSocketService.isConnected()) {
-			try {
-				await WebSocketService.connect('ws://localhost:8080/ws');
-			} catch (error) {
-				console.error('Failed to connect to WebSocket:', error);
-				// 에러 처리
-			}
-		}
-	} else if (websocketRoutes.includes(from.name) && !websocketRoutes.includes(to.name)) {
-		WebSocketService.close();
-	}
-	next();
+    if (websocketRoutes.includes(to.name)) {
+        if (!WebSocketService.isConnected()) {
+            try {
+                await WebSocketService.connect('ws://localhost:8080/ws');
+            } catch (error) {
+                console.error('Failed to connect to WebSocket:', error);
+                // 에러 처리
+            }
+        }
+    } else if (websocketRoutes.includes(from.name) && !websocketRoutes.includes(to.name)) {
+        WebSocketService.close();
+    }
+    next();
 });
 
 export default router;
