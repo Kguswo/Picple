3246288708a7d30afafbd3967fe728974246a094
염규영 <yeomky98@naver.com>
2024-08-08 19:31:23 +0900

    [#52] comment: add comments to background service

--- backend/src/main/java/com/ssafy/picple/domain/background/service/BackgroundServiceImpl.java
+++ backend/src/main/java/com/ssafy/picple/domain/background/service/BackgroundServiceImpl.java
@@ -33,16 +33,20 @@ public class BackgroundServiceImpl implements BackgroundService {
 	private final BackgroundUserRepository backgroundUserRepository;
 	private final FileUploadService fileUploadService;
 
+	// 기본 배경 사진을 DB에서 조회하여 반환
 	@Override
 	public List<BackgroundResponseDto> getDefaultBackgrounds() throws BaseException {
 		return getBackgroundsBy(() -> backgroundRepository.findByIsDefault(true));
 	}
 
+	// 특정 사용자가 추가한 배경 사진을 DB에서 조회하여 반환
 	@Override
 	public List<BackgroundResponseDto> getUserBackgrounds(Long userId) throws BaseException {
 		return getBackgroundsBy(() -> backgroundRepository.findByUserId(userId));
 	}
 
+	// 배경 사진 리스트를 조회하고, BackgroundResponseDto로 변환
+	// getDefaultBackgrounds와 getUserBackgrounds에서 쓰임
 	private List<BackgroundResponseDto> getBackgroundsBy(Supplier<List<Background>> backgroundSupplier) throws
 			BaseException {
 		try {
@@ -55,12 +59,14 @@ public class BackgroundServiceImpl implements BackgroundService {
 		}
 	}
 
+	// AI를 통해 배경 사진을 생성하여 S3와 DB에 저장
 	@Override
 	@Transactional
 	public void createAIBackground(Long userId, String prompt) throws BaseException {
 		try {
 			// 프롬프트를 통해 AI API를 사용하여 사진 생성
 			String[] result = openAIService.createAIBackground(prompt);
+			s3Service.uploadBase64ImageToS3(result[0], result[1]);
 			saveBackgroundToDB(userId, result[0], result[1]);
 
 		} catch (Exception e) {
@@ -69,7 +75,7 @@ public class BackgroundServiceImpl implements BackgroundService {
 		}
 	}
 
-	// 테스트 필요
+	// 로컬에서 업로드된 파일을 S3와 DB에 저장
 	@Override
 	@Transactional
 	public void createLocalBackground(Long userId, MultipartFile file) throws BaseException {
@@ -83,6 +89,7 @@ public class BackgroundServiceImpl implements BackgroundService {
 		}
 	}
 
+	// 배경 사진의 정보, 사용자와 배경 사진의 관계를 DB에 저장
 	private void saveBackgroundToDB(Long userId, String backgroundUrl, String fileName) throws BaseException {
 		// 데이터베이스에 저장
 		Background background = Background.builder()
@@ -102,6 +109,7 @@ public class BackgroundServiceImpl implements BackgroundService {
 		backgroundUserRepository.save(backgroundUser);
 	}
 
+	// 배경 사진을 DB에서 삭제
 	@Override
 	@Transactional
 	public void deleteBackground(Long backgroundId, Long userId) throws BaseException {
