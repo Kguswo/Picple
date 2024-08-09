d2ab41367a85789e03f1644be8f6196166c4d889
염규영 <yeomky98@naver.com>
2024-08-09 09:50:23 +0900

    [#52] comment: add comments to user request

--- backend/src/main/java/com/ssafy/picple/domain/user/dto/request/LoginRequest.java
+++ backend/src/main/java/com/ssafy/picple/domain/user/dto/request/LoginRequest.java
@@ -4,10 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 로그인 요청을 위한 DTO
+ * 사용자가 이메일과 비밀번호로 로그인 시 사용
+ */
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
-    private String email;
-    private String password;
+	private String email;
+	private String password;
 }
--- backend/src/main/java/com/ssafy/picple/domain/user/dto/request/ModifyPasswordRequest.java
+++ backend/src/main/java/com/ssafy/picple/domain/user/dto/request/ModifyPasswordRequest.java
@@ -5,11 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * 비밀번호 변경 요청을 위한 DTO
+ * 사용자가 기존, 새 비밀번호 입력 시 사용
+ */
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ModifyPasswordRequest {
-    private String oldPassword;
-    private String newPassword;
+	private String oldPassword;
+	private String newPassword;
 }
