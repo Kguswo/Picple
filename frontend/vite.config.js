d8ca55f52b40cada20740877939fe0ba6a203de8
김예훈 <rladpgns00@gmail.com>
2024-08-09 10:37:12 +0900

    hotfix: 배포 환경에서 로그 제거

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,4 +14,14 @@ export default defineConfig({
 	build: {
 		sourcemap: false,
 	},
+
+	build: {
+		minify: 'terser',
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+			},
+		},
+	},
 });
