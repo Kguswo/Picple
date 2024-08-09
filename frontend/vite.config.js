e0a7989b7cecbf81c97efd09c53a052564ff74d7
김예훈 <rladpgns00@gmail.com>
2024-08-09 14:45:22 +0900

    Fix merge conflicts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-    plugins: [vue(), vueDevTools()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
-    devServer: {
-        https: true,
-    },
+	plugins: [vue(), vueDevTools()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	build: {
+		sourcemap: false,
+	},
 });
