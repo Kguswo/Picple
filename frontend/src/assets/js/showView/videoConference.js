5c4128ec74f9ccfd1266a185e2c70206fbaabaae
유병주 <dbqudwn011@naver.com>
2024-08-12 17:23:23 +0900

    fix: videoConf

--- frontend/src/assets/js/showView/videoConference.js
+++ frontend/src/assets/js/showView/videoConference.js
@@ -122,7 +122,13 @@ export const applySegmentation = async (streamRef) => {
         console.log('4. videoElement 생성 및 재생 시작');
 
         console.log('5. SelfieSegmentation 초기화 시작');
-        const { SelfieSegmentation } = await import('@mediapipe/selfie_segmentation');
+        const SelfieSegmentation = (await import('@mediapipe/selfie_segmentation')).SelfieSegmentation;
+        console.log('SelfieSegmentation loaded:', SelfieSegmentation);
+        
+        if (typeof SelfieSegmentation !== 'function') {
+            throw new Error('SelfieSegmentation is not a constructor');
+        }
+        
         const selfieSegmentation = new SelfieSegmentation({
             locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`
         });
