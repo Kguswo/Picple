95ff52d5ebd849758c48792ff8446a5e8893540d
염규영 <yeomky98@naver.com>
2024-08-12 18:04:21 +0900

    [#50] fix: remove unused repository

--- backend/src/main/java/com/ssafy/picple/domain/chat/repository/ChatMessageRepository.java
+++ backend/src/main/java/com/ssafy/picple/domain/chat/repository/ChatMessageRepository.java
@@ -1,8 +1,10 @@
 package com.ssafy.picple.domain.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ssafy.picple.domain.chat.entity.ChatMessage;
 
-public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
+@Repository
+public interface ChatMessageRepository {
 }
