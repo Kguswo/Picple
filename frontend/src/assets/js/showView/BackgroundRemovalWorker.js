35d58299ac78126418df52352dd08b8f3d414e62
ByeongJu <dbqudwn011@naver.com>
2024-08-14 23:50:19 +0900

    fix: modify Webworker code

--- frontend/src/assets/js/showView/BackgroundRemovalWorker.js
+++ frontend/src/assets/js/showView/BackgroundRemovalWorker.js
@@ -1,19 +1,37 @@
-importScripts('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1.1632777926/selfie_segmentation.js');
-
 let selfieSegmentation;
+
+async function fetchAndCreateBlobUrl(url) {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  return URL.createObjectURL(blob);
+}
 
 self.onmessage = async function(e) {
   if (e.data.type === 'init') {
-    selfieSegmentation = new self.SelfieSegmentation({locateFile: (file) => {
-      return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
-    }});
-    selfieSegmentation.setOptions({
-      modelSelection: 1,
-    });
-    await selfieSegmentation.initialize();
-    self.postMessage({ type: 'ready' });
+    try {
+      const scriptUrl = await fetchAndCreateBlobUrl('https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation@0.1.1632777926/selfie_segmentation.js');
+      importScripts(scriptUrl);
+
+      selfieSegmentation = new self.SelfieSegmentation({locateFile: async (file) => {
+        const fileUrl = `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+        return await fetchAndCreateBlobUrl(fileUrl);
+      }});
+
+      selfieSegmentation.setOptions({
+        modelSelection: 1,
+      });
+
+      await selfieSegmentation.initialize();
+      self.postMessage({ type: 'ready' });
+    } catch (error) {
+      self.postMessage({ type: 'error', message: error.message });
+    }
   } else if (e.data.type === 'segment') {
-    const results = await selfieSegmentation.send({image: e.data.imageData});
-    self.postMessage({ type: 'segmentation', segmentation: results.segmentationMask }, [results.segmentationMask]);
+    try {
+      const results = await selfieSegmentation.send({image: e.data.imageData});
+      self.postMessage({ type: 'segmentation', segmentation: results.segmentationMask }, [results.segmentationMask]);
+    } catch (error) {
+      self.postMessage({ type: 'error', message: error.message });
+    }
   }
 };
